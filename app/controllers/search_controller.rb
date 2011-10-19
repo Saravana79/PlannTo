b0d759c46ea9f4b595d5e04b893b3b267181309e
Ganesh Prasad <ganeshprasadsr@gmail.com>
2011-10-19 21:06:50 +0530

    search page.

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,12 +1,55 @@
 class SearchController < ApplicationController
 
+  def index
+    @page  = params[:page].nil? ? 1 : params[:page].to_i
+    @manufacturer  = params[:manufacturer].blank? ? "" : params[:manufacturer]
+    @min_price = params[:min_price].blank? ? '' : params[:min_price]
+    @max_price = params[:max_price].blank? ? '' : params[:max_price]
+    @fuel_type = params[:fuel_type].blank? ? "" : params[:fuel_type]
+
+    @items = Sunspot.search(Car) do
+      #keywords "Aston", :fields => :name
+      with :manufacturer, params[:manufacturer] if !params[:manufacturer].blank?
+      facet :manufacturer
+      dynamic :features do                
+        with(:Price).greater_than(params[:min_price]) if !params[:min_price].blank?
+        with(:Price).less_than(params[:max_price]) if !params[:max_price].blank?        
+        # with(:'Fuel Economy [City]', 12.4)
+        facet(:Engine)
+        facet(:'Fuel Economy [City]')      
+      end
+
+      dynamic :features_string do
+        with('Fuel Type'.to_sym, params[:fuel_type]) if !params[:fuel_type].blank?
+      end
+
+      paginate(:page => params[:page], :per_page => 10)
+    end
+    #order_by :class , :desc    
+
+    @min_price = 2500000 if @min_price.blank?
+    @max_price = 5000000 if @max_price.blank?
+    @petrol  = true if @fuel_type == "Petrol"
+    @diesel = true if @fuel_type == "Diesel"
+    @both  = true if @fuel_type == ""
+
+    if @items.results.count < 10
+      @display = "none;"
+    else
+      @display = "block;"
+      @page += 1
+    end
+
+  end
+
   def search_items
     @page  = params[:page].nil? ? 1 : params[:page].to_i
 
     @items = Sunspot.search(CarGroup, Manufacturer) do
-      keywords params[:q]
+      keywords params[:q], :fields => :name 
       paginate(:page => params[:page], :per_page => 10)
-      # order_by :itemtype_id, :asc
+      #facet :types
+      order_by :class , :desc
     end
     if @items.results.count < 10
       @display = "none;"
@@ -20,9 +63,9 @@ class SearchController < ApplicationController
 
   def autocomplete_items
     @items = Sunspot.search(CarGroup, Manufacturer) do
-      keywords params[:term]
+      keywords params[:term], :fields => :name
       paginate(:page => 1, :per_page => 6)
-      # order_by :itemtype_id, :asc
+      order_by :class , :desc
     end
     
 
@@ -42,6 +85,15 @@ class SearchController < ApplicationController
     #        @results  << {:id => item.cargroup.id, :value => "#{item.cargroup.name}", :imgsrc => "/images/B16.jpg", :header => true, :children => [{:id => item.id, :value => "#{item.name}", :imgsrc => "/images/B16.jpg", :header => false }]} if @added == false
     #      end
     #    end
-    render :json => @items.results.collect{|item| {:id => item.id, :value => "#{item.name}", :imgsrc => "/images/B16.jpg", :type => item.type }}
+    render :json => @items.results.collect{|item| {:id => item.id, :value => "#{item.name}", :imgsrc => "/images/B16.jpg", :type => item.type.humanize }}
+  end
+
+  def autocomplete_manufacturers
+    @items = Sunspot.search(Manufacturer) do
+      keywords params[:term], :fields => :name
+      #paginate(:page => 1, :per_page => 6)
+    end
+
+    render :json => @items.results.collect{|item| {:id => item.id, :value => "#{item.name}"}}
   end
 end
--- app/models/car.rb
+++ app/models/car.rb
@@ -4,21 +4,37 @@
 class Car < Product
   has_many :itemrelationships, :foreign_key => :item_id
   has_many :relatedcars,
-           :through => :itemrelationships
+    :through => :itemrelationships, :include => :cargroup
   acts_as_taggable
-#  acts_as_taggable_on :product
+  #  acts_as_taggable_on :product
   acts_as_commentable
   
-    searchable :auto_index => true, :auto_remove => true  do
-      string :manufacturer do |product|
-        product.manufacturer.name    
-      end 
-      dynamic_string :features do |car|
-        car.attribute_values.inject({}) do |hash,attribute_value|
+  searchable :auto_index => true, :auto_remove => true  do
+    text :name , :boost => 4.0,  :as => :name_ac
+    string :manufacturer do |product|
+      product.manufacturer.name
+    end
+      
+    dynamic_float :features do |car|
+      car.attribute_values.inject({}) do |hash,attribute_value|
+        if attribute_value.attribute.attribute_type == "Numeric"
           hash.merge(attribute_value.attribute.name.to_sym => attribute_value.value)
+        else
+          hash
         end
       end
     end
-  
+    
+    dynamic_string :features_string do |car|
+      car.attribute_values.inject({}) do |hash,attribute_value|
+        if attribute_value.attribute.attribute_type == "Text"
+          hash.merge(attribute_value.attribute.name.to_sym => attribute_value.value)
+        else
+          hash
+        end
+      end
+    end
+  end
+
   
 end
