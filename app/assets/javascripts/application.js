c0b33aa863466b287324b4ca83731de131a8d063
Vishnu Atrai <me@vishnuatrai.com>
2011-11-19 15:26:25 +0530

    replace with check box

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -7,3 +7,29 @@
 //= require jquery
 //= require jquery_ujs
 //= require_tree .
+
+
+function removeCompare(obj){
+      $(obj).parents('.compare-view').remove();
+      key = $(obj).attr('rel') ;
+      $.cookie(key,null);
+      $('#add_to_compare_' + key ).attr('checked',false);
+      var url = $('#compare-items a').attr('href')
+      idsPrt = url.split('=');
+      idsStr = idsPrt[1];
+      var url = url.split('=')[0];
+      idsArr = idsStr.split(',');
+      ids = jQuery.grep(idsArr,function(a){return a != key } );
+      idsStr = ids.join(',')
+      $.cookie('allItems',idsStr)
+      url = $('#compare-items a').attr('href',url + '=' + idsStr)
+      if($('#compare-list .compare-view').length > 1){
+        $('#compare-items').show();
+      }else{
+        $('#compare-items').hide();
+      }
+      if($('#compare-list div.compare-view').length == 0){
+        $('#compare-list').hide();
+      }
+      return false;
+  };
