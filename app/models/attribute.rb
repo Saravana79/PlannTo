93d7f92cbd80a2f592a93c69e32cb0727a1e6255
Ganesh Prasad <ganeshprasadsr@gmail.com>
2011-11-26 10:19:13 +0530

    search display attributes.

--- app/models/attribute.rb
+++ app/models/attribute.rb
@@ -1,6 +1,7 @@
 class Attribute < ActiveRecord::Base
   has_many :attribute_values
   has_many :items, :through => :attribute_values
+  has_many :search_display_attributes, :class_name => "SearchAttribute"
 
   #belongs_to :user, :foreign_key => 'created_by'
   
