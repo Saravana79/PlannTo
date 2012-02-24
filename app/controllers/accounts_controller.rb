fc66e4c88ef7d7f8e541a5712b5804d57ed77ebf
Mitesh Jain <mitijain123@gmail.com>
2012-02-24 22:32:10 +0530

    some refator to make generic

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -24,8 +24,9 @@ class AccountsController < ApplicationController
   end
 
   def profile
-    require 'will_paginate/array'    
-    @follow_item = Follow.for_follower(current_user).where(:followable_type => ['Car', 'Manufacturer', 'CarGroup', 'Mobile']).group_by(&:followable_type)
+    require 'will_paginate/array'
+
+    @follow_item = Follow.for_follower(current_user).where(:followable_type => get_followable_types(params[:follow])).group_by(&:followable_type)
     @car_items = get_array(@follow_item['Car']) + get_array(@follow_item['CarGroup']) + get_array(@follow_item['Manufacturer'])
     @car_items = @car_items.paginate(:page => params["page"], :per_page => 8) rescue [].paginate
     render :layout => "product"
@@ -34,5 +35,14 @@ class AccountsController < ApplicationController
   private
   def get_array(array_obj)
     array_obj.blank? ?  [] : array_obj
+  end
+
+  def get_followable_types(item_type)
+    case item_type
+      when 'Car'
+        ['Car', 'Manufacturer', 'CarGroup']
+      when 'Mobile'
+        ['Mobile']      
+    end
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120224133423) do
+ActiveRecord::Schema.define(:version => 20120222170227) do
 
   create_table "answers", :force => true do |t|
     t.integer  "question_id"
@@ -216,6 +216,11 @@ ActiveRecord::Schema.define(:version => 20120224133423) do
     t.string   "message",     :limit => 2000
     t.string   "token",                       :null => false
     t.string   "user_ip",                     :null => false
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "itemdetails", :force => true do |t|
     t.datetime "created_at"
     t.datetime "updated_at"
   end
@@ -480,14 +485,21 @@ ActiveRecord::Schema.define(:version => 20120224133423) do
     t.datetime "updated_at"
     t.integer  "reputation",                            :default => 0,  :null => false
     t.string   "name"
+    t.integer  "facebook_id"
+    t.string   "photo_file_name"
+    t.string   "photo_content_type"
+    t.integer  "photo_file_size"
+    t.datetime "photo_updated_at"
+    t.integer  "reputation",                            :default => 0,  :null => false
     t.integer  "invitation_id"
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
   add_index "users", ["reset_password_token"], :name => "index_users_on_reset_password_token", :unique => true
 
-  create_table "video_contents", :force => true do |t|
-    t.string "youtube"
+  create_table "vendors", :force => true do |t|
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   create_table "vote_counts", :force => true do |t|
