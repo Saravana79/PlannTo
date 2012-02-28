2418a80461a948f3d089d5d4b475bb19df22ffbc
Mitesh Jain <mitijain123@gmail.com>
2012-02-28 21:52:16 +0530

    profile page for mobile tab

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -26,23 +26,22 @@ class AccountsController < ApplicationController
   def profile
     require 'will_paginate/array'
 
-    @follow_item = Follow.for_follower(current_user).where(:followable_type => get_followable_types(params[:follow])).group_by(&:followable_type)
-    @car_items = get_array(@follow_item['Car']) + get_array(@follow_item['CarGroup']) + get_array(@follow_item['Manufacturer'])
-    @car_items = @car_items.paginate(:page => params["page"], :per_page => 8) rescue [].paginate
-    render :layout => "product"
+    @follow_types = get_followable_types(params[:follow])
+    @follow_item = Follow.for_follower(current_user).where(:followable_type => @follow_types).group_by(&:followable_type)
+    respond_to do|format|      
+      format.html {render :layout => "product"}
+    end
   end
 
   private
-  def get_array(array_obj)
-    array_obj.blank? ?  [] : array_obj
-  end
-
   def get_followable_types(item_type)
     case item_type
       when 'Car'
         ['Car', 'Manufacturer', 'CarGroup']
       when 'Mobile'
-        ['Mobile']      
+        ['Mobile']
+      else
+        ['Car', 'Manufacturer', 'CarGroup']
     end
   end
 end
