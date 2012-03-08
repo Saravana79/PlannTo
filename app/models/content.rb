e430fa3b11831e8ea61b7742ad9db71706c69dfc
Vivek Aditya <vivekg342@gmail.com>
2012-03-09 00:40:16 +0530

    Related items - adding again

--- app/models/content.rb
+++ app/models/content.rb
@@ -14,27 +14,28 @@ class Content < ActiveRecord::Base
 
  def self.filter(options)
    if !options.blank?
-   options.inject(self) do |scope, (key, value)|
-     return scope if value.blank?
-     value= value.join(",") if is_a?(Array)
-     case key.to_sym
-     when :items
-       scope.scoped(:conditions => ['content_item_relations.item_id in (?)', value ], :joins => :content_item_relations)
-     when :type
-       scope.scoped(:conditions => ["#{self.table_name}.type in (?)", value ])
-     when :order
-       attribute, order = value.split(" ") 
-       scope.scoped(:order => "#{self.table_name}.#{attribute} #{order}")
-     when :limit
-       scope.limit(value)
-     else 
-       scope
-     end
-   end 
- else
-   Content.limit(10)
+     options.inject(self) do |scope, (key, value)|
+       return scope if value.blank?
+       value= value.join(",") if is_a?(Array)
+       case key.to_sym
+       when :items
+         all_items = Item.get_all_related_items_ids(value)
+         scope.scoped(:conditions => ['content_item_relations.item_id in (?)', all_items ], :joins => :content_item_relations)
+       when :type
+         scope.scoped(:conditions => ["#{self.table_name}.type in (?)", value ])
+       when :order
+         attribute, order = value.split(" ") 
+         scope.scoped(:order => "#{self.table_name}.#{attribute} #{order}")
+       when :limit
+         scope.limit(value)
+       else 
+         scope
+       end
+     end 
+   else
+     Content.limit(10)
+   end
  end
-  end
   
 	def save_with_items!(items)
 	  Content.transaction do
