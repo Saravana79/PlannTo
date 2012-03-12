911e0ed41b87e275c063f809ebb2115e14dc2280
Ganesh Prasad <ganeshprasadsr@gmail.com>
2012-03-12 23:06:35 +0530

    vote count cache.

--- app/models/content.rb
+++ app/models/content.rb
@@ -9,33 +9,46 @@ class Content < ActiveRecord::Base
 	has_many :content_item_relations
   has_many :items, :through => :content_item_relations
   belongs_to :itemtype
- scope :item_contents, lambda { |item_id| joins(:content_item_relations).where('content_item_relations.item_id = ?', item_id)}
+  scope :item_contents, lambda { |item_id| joins(:content_item_relations).where('content_item_relations.item_id = ?', item_id)}
 
 
- def self.filter(options)
-   if !options.blank?
-     options.inject(self) do |scope, (key, value)|
-       return scope if value.blank?
-       value= value.join(",") if is_a?(Array)
-       case key.to_sym
-       when :items
-         all_items = Item.get_all_related_items_ids(value)
-         scope.scoped(:conditions => ['content_item_relations.item_id in (?)', all_items ], :joins => :content_item_relations).uniq
-       when :type
-         scope.scoped(:conditions => ["#{self.table_name}.type in (?)", value ])
-       when :order
-         attribute, order = value.split(" ") 
-         scope.scoped(:order => "#{self.table_name}.#{attribute} #{order}")
-       when :limit
-         scope.limit(value)
-       else 
-         scope
-       end
-     end 
-   else
-     Content.limit(10)
-   end
- end
+  def content_vote_count
+    count = $redis.get("#{VoteCount::REDIS_CONTENT_VOTE_KEY_PREFIX}#{self.id}")
+    if count.nil?
+      vote = VoteCount.search_vote(self).first
+      count = vote.nil? ? 0 : (vote.vote_count_positive - vote.vote_count_negative)
+      $redis.set("#{VoteCount::REDIS_CONTENT_VOTE_KEY_PREFIX}#{self.id}", count)
+      return count
+    else
+      return count
+    end
+  end
+
+
+  def self.filter(options)
+    if !options.blank?
+      options.inject(self) do |scope, (key, value)|
+        return scope if value.blank?
+        value= value.join(",") if is_a?(Array)
+        case key.to_sym
+        when :items
+          all_items = Item.get_all_related_items_ids(value)
+          scope.scoped(:conditions => ['content_item_relations.item_id in (?)', all_items ], :joins => :content_item_relations).uniq
+        when :type
+          scope.scoped(:conditions => ["#{self.table_name}.type in (?)", value ])
+        when :order
+          attribute, order = value.split(" ")
+          scope.scoped(:order => "#{self.table_name}.#{attribute} #{order}")
+        when :limit
+          scope.limit(value)
+        else
+          scope
+        end
+      end
+    else
+      Content.limit(10)
+    end
+  end
   
 	def save_with_items!(items)
 	  Content.transaction do
--- app/models/vote_count.rb
+++ app/models/vote_count.rb
@@ -1,10 +1,16 @@
 class VoteCount < ActiveRecord::Base
+  after_save :update_cache
   require 'global_utilities'
-
+  REDIS_CONTENT_VOTE_KEY_PREFIX = "content_vote_count_"
   # scope :order_by_votes, lambda { |*args| where(["voteable_type = ?", args.first.class.name]).order(:vote_count) }
   scope :search_vote, lambda {|voteable| where("voteable_id = ? and voteable_type = ?", voteable.id, GlobalUtilities.get_class_name(voteable.class.name)) }
 
 
   belongs_to :voter, :polymorphic => true
   belongs_to :voteable, :polymorphic => true
+
+  def update_cache
+    count = self.vote_count_positive - self.vote_count_negative
+    $redis.set("#{REDIS_CONTENT_VOTE_KEY_PREFIX}#{self.voteable_id}", count)
+  end
 end
