e23ad71f8bfb9a6f83af84ccf14d3b2d911741b6
Ganesh Prasad <ganeshprasadsr@gmail.com>
2012-03-23 21:16:55 +0530

    search car fix for other pages.

--- app/assets/javascripts/preferences.js
+++ app/assets/javascripts/preferences.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-$("#search_car").autocomplete({
+    $("#search_car").autocomplete({
         minLength:2,
         format:"js",
         // source: "/search/autocomplete_items?search_type=" + $("#plannto_search_type").val() ,
@@ -15,62 +15,64 @@ $("#search_car").autocomplete({
                 shadow:true, // Whether to render a shadow
                 hwaccel:false // Whether to use hardware acceleration
             };
-//       var target = document.getElementById('search_car');
-//       var spinner = new Spinner(opts).spin(target);
+            //       var target = document.getElementById('search_car');
+            //       var spinner = new Spinner(opts).spin(target);
             $.ajax(
-                {
-                    url:"/search/autocomplete_items",
-                    data:{
-                        term:$("#search_car").val(),
-                        search_type:$("#category").val(),
-                        authenticity_token: window._token,
-                        from_profile: true
-                    },
-                    type:"GET",
-                    dataType:"json",
-                    success:function (data) {
-                        response($.map(data, function (item) {
-                            return{
-                                id:item.id,
-                                value:item.value,
-                                imgsrc:item.imgsrc,
-                                type:item.type,
-                                url:item.url
-                            }
-                        }));
-//           spinner.stop();
-                    }
-                });
+            {
+                url:"/search/autocomplete_items",
+                data:{
+                    term:$("#search_car").val(),
+                    search_type:$("#category").val(),
+                    authenticity_token: window._token,
+                    from_profile: true
+                },
+                type:"GET",
+                dataType:"json",
+                success:function (data) {
+                    response($.map(data, function (item) {
+                        return{
+                            id:item.id,
+                            value:item.value,
+                            imgsrc:item.imgsrc,
+                            type:item.type,
+                            url:item.url
+                        }
+                    }));
+                //           spinner.stop();
+                }
+            });
         },
         focus:function (e, ui) {
             return false
         },
         select: function( event, ui ) {
             $.ajax(
-                {
-                    url:"/preferences/plan_to_buy?item_id="+ui.item.id+"&follow_type="+ "Buyer&buying_plan_id=" + $("#buying_plan_id").val(), 
-                    type:"GET",
-                    dataType:"script"
-                });
+            {
+                url:"/preferences/plan_to_buy?item_id="+ui.item.id+"&follow_type="+ "Buyer&buying_plan_id=" + $("#buying_plan_id").val(),
+                type:"GET",
+                dataType:"script"
+            });
         }
     })
+    if ($("#search_car").index() != -1) {
         $("#search_car").data("autocomplete")._renderItem = function (ul, item, index) {
-        if (index == -1) {
-            return $("<li></li>")
+            if (index == -1) {
+                return $("<li></li>")
                 .data("item.autocomplete", item)
                 .append("<a class='searchMore'>" + item.value + "" + "</a>")
                 .appendTo(ul);
-        }
-        else {
-            return $("<li></li>")
+            }
+            else {
+                return $("<li></li>")
                 .data("item.autocomplete", item)
                 .append('<a>' + "<div style='margin-left:5px;float:left'><img width='40' height='40' src='" + item.imgsrc + "' /></div>" + "<div style='margin-left:53px;'><span class='atext'>" + item.value + "</span><br/><span class ='atext'>" + item.type + "</span></div></a>")
                 .appendTo(ul);
+            }
         }
     }
 
 
-$('#plantobuyPaginate div.pagination a').live('click', function(){
+    $('#plantobuyPaginate div.pagination a').live('click', function(){
         var page = $(this).text()
         var current = $('em.current').text();
         if (page == "← Previous"){
@@ -81,11 +83,11 @@ $('#plantobuyPaginate div.pagination a').live('click', function(){
         }
 
         $.ajax(
-                {
-                    url:"/preferences/plan_to_buy?follow_type=" + ""+ "&buying_plan_id=" + $("#buying_plan_id").val() + "&page=" + page, 
-                    type:"GET",
-                    dataType:"script"
-                });
+        {
+            url:"/preferences/plan_to_buy?follow_type=" + ""+ "&buying_plan_id=" + $("#buying_plan_id").val() + "&page=" + page,
+            type:"GET",
+            dataType:"script"
+        });
         
         return false;
     })
