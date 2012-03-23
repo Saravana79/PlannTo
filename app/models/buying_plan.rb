e5fd58a5247d87824913492bf2db451bb18e0b37
Ganesh Prasad <ganeshprasadsr@gmail.com>
2012-03-23 20:04:09 +0530

    voting for buying plan and displaying avatars.

--- app/models/buying_plan.rb
+++ app/models/buying_plan.rb
@@ -4,4 +4,16 @@ class BuyingPlan < ActiveRecord::Base
   has_one :user_question
   belongs_to :user
   belongs_to :itemtype
+
+  def content_vote_count
+    count = $redis.get("#{VoteCount::REDIS_BUYING_PLAN_VOTE_KEY_PREFIX}#{self.id}")
+    if count.nil?
+      vote = VoteCount.search_vote(self).first
+      count = vote.nil? ? 0 : (vote.vote_count_positive - vote.vote_count_negative)
+      $redis.set("#{VoteCount::REDIS_BUYING_PLAN_VOTE_KEY_PREFIX}#{self.id}", count)
+      return count
+    else
+      return count
+    end
+  end
 end
--- app/models/vote_count.rb
+++ app/models/vote_count.rb
@@ -2,6 +2,7 @@ class VoteCount < ActiveRecord::Base
   after_save :update_cache
   require 'global_utilities'
   REDIS_CONTENT_VOTE_KEY_PREFIX = "content_vote_count_"
+  REDIS_BUYING_PLAN_VOTE_KEY_PREFIX = "buying_plan_vote_count_"
   # scope :order_by_votes, lambda { |*args| where(["voteable_type = ?", args.first.class.name]).order(:vote_count) }
   scope :search_vote, lambda {|voteable| where("voteable_id = ? and voteable_type = ?", voteable.id, GlobalUtilities.get_class_name(voteable.class.name)) }
 
@@ -11,6 +12,14 @@ class VoteCount < ActiveRecord::Base
 
   def update_cache
     count = self.vote_count_positive - self.vote_count_negative
-    $redis.set("#{REDIS_CONTENT_VOTE_KEY_PREFIX}#{self.voteable_id}", count)
+    redis_key_prefix = get_cache_prefix
+    $redis.set("#{redis_key_prefix}#{self.voteable_id}", count)
+  end
+
+  def get_cache_prefix
+    case GlobalUtilities.get_class_name(voteable.class.name)
+    when "Content" then REDIS_CONTENT_VOTE_KEY_PREFIX
+    when "BuyingPlan" then REDIS_BUYING_PLAN_VOTE_KEY_PREFIX
+    end
   end
 end
