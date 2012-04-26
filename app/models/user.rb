e48c16267c6e946821f8f4027a842cb0ce501968
Mitesh Jain <mjain@crri.co.in>
2012-04-26 23:03:21 +0530

    making the avatar upload work in the account settings page

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,7 +10,7 @@ class User < ActiveRecord::Base
                           Follow::ProductFollowType::Follow => "follow_item_ids",
                           Follow::ProductFollowType::Buyer => "buyer_item_ids"}
   # Setup accessible (or protected) attributes for your model
-  attr_accessible :email, :password, :password_confirmation, :name, :remember_me, :facebook_id, :invitation_id, :invitation_token
+  attr_accessible :email, :password, :password_confirmation, :name, :remember_me, :facebook_id, :invitation_id, :invitation_token, :avatar
   attr_accessor :follow_type
   acts_as_followable
   acts_as_follower
