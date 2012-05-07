8cb25ca5a0447cb8dcc95bc51aaafd7ee0b62edc
Ganesh Prasad <ganeshprasadsr@gmail.com>
2012-05-07 19:56:43 +0530

    uncommented view migration

--- db/migrate/20120503161935_add_video_to_article_content.rb
+++ db/migrate/20120503161935_add_video_to_article_content.rb
@@ -1,7 +1,7 @@
 class AddVideoToArticleContent < ActiveRecord::Migration
   def change
-   # drop_citier_view(ArticleContent)
-    #add_column :article_contents, :video, :boolean
+    drop_citier_view(ArticleContent)
+    add_column :article_contents, :video, :boolean
      create_citier_view(ArticleContent)
   end
 end
