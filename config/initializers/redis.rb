7e7caf42ae8ba1b906ebcf36554980101b42ca38
Saravana79 <saravanak@gmail.com>
2012-05-12 21:48:41 +0530

    test

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,4 +1,7 @@
-$redis = Redis.new(:host => 'localhost', :port => 6379)
+uri = URI.parse(ENV["REDISTOGO_URL"])
+REDIS = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
+
+#$redis = Redis.new(:host => 'localhost', :port => 6379)
 #Resque::Server.use(Rack::Auth::Basic) do |user, password|
 #  #password == "secret"
 #end
