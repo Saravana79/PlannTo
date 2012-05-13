8fa1cd6bb48dddfd75d1c560d02fc7c27ee147cc
Ganesh Prasad <ganeshprasadsr@gmail.com>
2012-05-13 09:56:05 +0530

    redis heroku changes.

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,4 +1,6 @@
-$redis = Redis.new(:host => 'localhost', :port => 6379)
+#$redis = Redis.new(:host => 'localhost', :port => 6379)
+uri = URI.parse(ENV["REDISTOGO_URL"])
+$redis = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
 #Resque::Server.use(Rack::Auth::Basic) do |user, password|
 #  #password == "secret"
 #end
