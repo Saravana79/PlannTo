fbdc6e5d9d57fb05d0a928f630fea737bedab1dc
Saravana79 <saravanak@gmail.com>
2012-05-18 20:54:59 +0530

    Added S3 related info

--- app/models/avatar.rb
+++ app/models/avatar.rb
@@ -1,5 +1,11 @@
 class Avatar < ActiveRecord::Base
   belongs_to :user
-  has_attached_file :photo, :styles => { :medium => "300x300>", :thumb => "100x100>", :small => "25x25>" }
+  has_attached_file :photo, 
+  :styles => { :medium => "300x300>", :thumb => "100x100>", :small => "25x25>" }
+   :storage => :s3,
+    :bucket => ENV['plannto'],
+    :s3_credentials => {
+      :access_key_id => ENV['AKIAJWDCN4DJWNL2FK5A'],
+      :secret_access_key => ENV['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']
 
 end
