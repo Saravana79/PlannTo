2d79626684422b6b3a61d173f33558f1b46d91ec
Saravana79 <saravanak@gmail.com>
2012-05-18 21:25:18 +0530

    Fixed the syntax issue.

--- app/models/avatar.rb
+++ app/models/avatar.rb
@@ -1,7 +1,7 @@
 class Avatar < ActiveRecord::Base
   belongs_to :user
   has_attached_file :photo, 
-  :styles => { :medium => "300x300>", :thumb => "100x100>", :small => "25x25>" }
+  :styles => { :medium => "300x300>", :thumb => "100x100>", :small => "25x25>" },
    :storage => :s3,
     :bucket => ENV['plannto'],
     :s3_credentials => {
