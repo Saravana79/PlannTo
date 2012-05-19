4d2704b83fd5e8180df56cc8e3ee0d5d387c0b4c
Saravana79 <saravanak@gmail.com>
2012-05-19 12:15:49 +0530

    Redis changed to production.

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,6 +1,6 @@
-$redis = Redis.new(:host => 'localhost', :port => 6379)
-#uri = URI.parse(ENV["REDISTOGO_URL"])
-#$redis = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
+#$redis = Redis.new(:host => 'localhost', :port => 6379)
+uri = URI.parse(ENV["REDISTOGO_URL"])
+$redis = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
 
 #Resque::Server.use(Rack::Auth::Basic) do |user, password|
 #  #password == "secret"
