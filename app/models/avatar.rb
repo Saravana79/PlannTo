72e807517bef451214d353d2fb46cfa15698298e
Saravana79 <saravanak@gmail.com>
2012-05-23 11:35:19 +0530

    Rolled back Amazon s3 changes.

--- app/models/avatar.rb
+++ app/models/avatar.rb
@@ -1,11 +1,12 @@
 class Avatar < ActiveRecord::Base
   belongs_to :user
   has_attached_file :photo, 
-  :styles => { :medium => "300x300>", :thumb => "100x100>", :small => "25x25>" },
-   :storage => :s3,
-    :bucket => ENV['plannto'],
-    :s3_credentials => {
-      :access_key_id => ENV['AKIAJWDCN4DJWNL2FK5A'],
-      :secret_access_key => ENV['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']
-      	}
+  :styles => { :medium => "300x300>", :thumb => "100x100>", :small => "25x25>" }
+  #,
+  # :storage => :s3,
+  #  :bucket => ENV['plannto'],
+  #  :s3_credentials => {
+  #    :access_key_id => ENV['AKIAJWDCN4DJWNL2FK5A'],
+  #    :secret_access_key => ENV['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']
+  #    	}
 end
