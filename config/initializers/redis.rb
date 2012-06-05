dd2df81071f09afcf14f327f887a42d9d90a555d
Saravana79 <saravanak@gmail.com>
2012-06-05 13:24:51 +0530

    Heroku fix for redis.

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -2,6 +2,7 @@
 uri = URI.parse(ENV["REDISTOGO_URL"])
 $redis = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
 
+Resque.redis = $redis
 #Resque::Server.use(Rack::Auth::Basic) do |user, password|
 #  #password == "secret"
 #end
