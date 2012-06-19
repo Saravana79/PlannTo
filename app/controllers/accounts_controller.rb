3959d25cb449c1e66764ff9bd46977bf6a4a6bec
Saravana79 <saravanak@gmail.com>
2012-06-19 11:52:07 +0530

    Fixed the profile page issue.

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -44,9 +44,9 @@ class AccountsController < ApplicationController
     require 'will_paginate/array'
 
     @follow_types = Itemtype.get_followable_types(params[:follow])
-    @itemtype=Itemtype.find_by_itemtype('Car')
+    @itemtype=Itemtype.find_by_itemtype(Itemtype.get_item_type_from_params(params[:follow]))
     @itemtype_id = @itemtype.id
-    @article_categories = ArticleCategory.by_itemtype_id(@itemtype_id).map { |e|[e.name, e.id]  } 
+    @article_categories = ArticleCategory.by_itemtype_id(@itemtype.id).map { |e|[e.name, e.id]  } 
     @follow_item = Follow.for_follower(current_user).where(:followable_type => @follow_types).group_by(&:followable_type)
     respond_to do|format|      
       format.html {render :layout => "product"}
