e2f6105e9633277e24ac5b82fc1fd40719c1c5aa
Saravana79 <saravanak@gmail.com>
2012-06-21 12:07:06 +0530

    Without tags, a content can be submitted. This created a problem in display.

--- app/helpers/contents_helper.rb
+++ app/helpers/contents_helper.rb
@@ -186,7 +186,9 @@ module ContentsHelper
 
   def get_anchor_name_link(content)
     first_item = content.items.first
-    content = "<a class='txt_brown' href=" + "/#{first_item.type.tableize}/#{first_item.id.to_s}" + "> #{first_item.name}</a> "
+    if (first_item?)
+        content = "<a class='txt_brown' href=" + "/#{first_item.type.tableize}/#{first_item.id.to_s}" + "> #{first_item.name}</a> "
+    end
     return content.html_safe
   end
 end
