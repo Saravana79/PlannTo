ade6004f3d8a8f910ce3cb2c010b40ce36fb3ba8
Saravana79 <saravanak@gmail.com>
2012-06-21 17:17:50 +0530

    Done image related changes.

--- app/models/content_photo.rb
+++ app/models/content_photo.rb
@@ -1,4 +1,8 @@
 class ContentPhoto < ActiveRecord::Base
   belongs_to :content
-  has_attached_file :photo,:styles => { :medium => "300x300>", :thumb => "100x100>" }
+  has_attached_file :photo,:styles => { :large => "560x360>", :thumb => "80x60>" },
+  :storage => :s3,
+  :bucket => ENV['plannto'],
+  :s3_credentials => "config/s3.yml",
+  :path => "images/content/:id/:style/:filename"
 end
