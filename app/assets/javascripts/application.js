83bd2ac39681b2f3f7eb5a52d6010c6b806ea8b6
Shanmukha <shanmukhakatta@gmail.com>
2012-06-21 20:40:20 +0530

    bug fixed due to assets precompile

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,4 +1,3 @@
-
 // This is a manifest file that'll be compiled into including all the files listed below.
 // Add new JavaScript/Coffee code in separate files in this directory and they'll automatically
 // be included in the compiled file accessible from http://example.com/assets/application.js
@@ -8,6 +7,7 @@
 //= require jquery-ui
 //= require_tree .
 //= require tinymce
+//= require jquery_ujs
 //= require jquery.remotipart
 
 
