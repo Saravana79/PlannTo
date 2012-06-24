5315a41402209da385f24d9ecfceb487ccee00b1
unknown <Ganesh@.(none)>
2012-06-24 13:29:38 +0530

    display error if signin fails.

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -12,15 +12,15 @@ class Users::SessionsController < Devise::SessionsController
 
   def create
 
-    logger.info "itscomehere--------------------------------------"
-    resource = warden.authenticate!(auth_options)
-    logger.info "itscomehere--------------------------------------"
+    # resource = warden.authenticate!(auth_options)
+    resource = warden.authenticate(auth_options)
+ # if resource
     unless request.xhr?
     set_flash_message(:notice, :signed_in) if is_navigational_format?
     sign_in(resource_name, resource)
     respond_with resource, :location => after_sign_in_path_for(resource)
     else
-      logger.info "-------------------------------------"
+   
       if user_signed_in?
         #ajax signed in
         $logged_in = true
@@ -30,18 +30,15 @@ class Users::SessionsController < Devise::SessionsController
         #ajax sign in failed.
       end
     end
-  end
-
-  def invalid
-    logger.info "it failed"
+  
   end
 
   def auth_options
     unless request.xhr?
     { :scope => resource_name, :recall => "#{controller_path}#new" }
     else
-      logger.info "#{controller_path}#invalid"
-      {:scope => resource_name, :recall => "#{controller_path}#invalid"}
+      logger.info "#{resource_name}"
+      {:scope => :user}
     end
   end
 
