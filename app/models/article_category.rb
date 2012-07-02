1e73790f55d1fd1d6e42bb8fa33d49cf85f4afec
Saravana79 <saravanak@gmail.com>
2012-07-02 20:22:58 +0530

    Changed howto/kb to guide.

--- app/models/article_category.rb
+++ app/models/article_category.rb
@@ -10,7 +10,7 @@ class ArticleCategory < ActiveRecord::Base
   ACCESSORIES = "Accessories"
   BOOKS = "Books"
   TRAVELOGUE = "Travelogue"
-  HOW_TO   = "HowTo/KB"
+  HOW_TO   = "HowTo/Guide"
   has_one :article_content
   belongs_to :itemtype
   validates_uniqueness_of :name, :scope => [:itemtype_id]
