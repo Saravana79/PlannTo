5aea5fbd59a6578cd99a13eb88ccc47504dffd29
Dinesh <dineshdhoom@gmail.com>
2012-07-06 00:58:22 +0530

    Fixing active tab issue under profile page

--- app/helpers/accounts_helper.rb
+++ app/helpers/accounts_helper.rb
@@ -10,4 +10,15 @@ module AccountsHelper
       follow_array[val].blank? ?  [] : follow_array[val]
     end.flatten
   end
+
+  def link_for_item_type(item_type, selected_item, user)
+    if item_type == selected_item || (selected_item.nil? && item_type=="Cars")
+      content_tag(:li,
+        link_to(content_tag(:span, item_type), profile_path(user.try(:username), :follow => item_type)),
+      :class => "tab_active")
+    else
+      content_tag(:li,
+        link_to(content_tag(:span, item_type), profile_path(user.try(:username), :follow => item_type)))
+    end
+  end
 end
