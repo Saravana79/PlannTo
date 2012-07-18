dfb7f385ab45af5ad7e3f6569acbd8340512f272
Saravana79 <saravanak@gmail.com>
2012-07-18 16:04:08 +0530

    Voter_id fix - Actually workaround as its not sure why voter_id fetch is coming as null.

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -31,7 +31,12 @@ class VotesController < ApplicationController
           voter.clear_votes(voteable)
         else
           logger.error "Vote #{ params[:vote]}"
-          voter.vote voteable,{:direction => params[:vote], :exclusive => true ,:id => current_user.fetch_vote(voteable).try(:id)}
+          if (params[:vote_id])
+            @vote_id = params[:vote_id]
+          else
+            @vote_id = current_user.fetch_vote(voteable).try(:id)
+          end 
+          voter.vote voteable,{:direction => params[:vote], :exclusive => true ,:id => @vote_id}
         end
       else
         voter.vote voteable,:direction => params[:vote]
