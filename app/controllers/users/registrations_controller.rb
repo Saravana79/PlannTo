789af77c20e3acd801e3ab3ca63cd62b0f76faea
Saravana79 <saravanak@gmail.com>
2012-07-26 12:53:48 +0530

    Fixed - Without invitiation new registration was failing

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -16,11 +16,15 @@ class Users::RegistrationsController < Devise::RegistrationsController
       @invitation.accept(resource) unless @invitation.blank?
       if resource.active_for_authentication?
         sign_in(resource_name, resource)
-        redirect_to @item.get_url || mobiles_url, notice: "Successfully registered"
       else
         expire_session_data_after_sign_in!
-        redirect_to @item.get_url || mobiles_url, notice: "Successfully registered"
       end
+      if @item.nil?
+        redirect_to  mobiles_url, notice: "Successfully registered"
+      else
+        redirect_to @item.get_url || mobiles_url, notice: "Successfully registered"
+      end 
+   
     else
       clean_up_passwords resource
       respond_with resource
