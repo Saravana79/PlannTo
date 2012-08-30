9fc1ab0906c4a5566f3e7e23ed0b16d395ddfa1e
Shanmukha <shanmukhakatta@gmail.com>
2012-08-30 18:54:34 +0530

    My feeds page render aftet login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -62,7 +62,7 @@ class ApplicationController < ActionController::Base
   
   #def after_sign_in_path_for(resource_or_scope)
     #profile_path(username: current_user.username)
-   # mobiles_path
+   # my_feeds_path
   #end
   
   def stored_location_for(resource_or_scope)
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,6 @@
 class HomeController < ApplicationController
   layout "product"
   def index
-    redirect_to mobiles_path if current_user
+    redirect_to my_feeds_path if current_user
   end
-
-end
+ end
