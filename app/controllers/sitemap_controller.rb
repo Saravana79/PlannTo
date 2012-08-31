03f2448b30b551c430f74b5c0675169ccaf882c9
Shanmukha <shanmukhakatta@gmail.com>
2012-08-31 12:38:47 +0530

    Site map

--- app/controllers/sitemap_controller.rb
+++ app/controllers/sitemap_controller.rb
@@ -1,8 +1,8 @@
 class SitemapController < ApplicationController
   def index
-   static_urls = [ {:url => 'http://localhost:3000/car/search',      :updated_at => ""}]
+   static_urls = [ {:url => 'http://wwww.plannto.com/car/search', :updated_at => ""}]
    @pages_to_visit  = static_urls
-   @pages_to_visit+= Item.all.collect{  |a| {:url => "http://localhost:3000" + item_path(a) ,  :updated_at => I18n.l(a.updated_at, :format => :w3c)} }
+   @pages_to_visit+= Item.all.collect{  |a| {:url => "http://www.plannto.com" + item_path(a) ,  :updated_at => I18n.l(a.updated_at, :format => :w3c)} }
  
     respond_to do |format|
       format.xml{ render :xml =>  @pages_to_visit}
