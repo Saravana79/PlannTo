592abf31cb0e1d97905511fd377407fd49a918ba
Ganesh <ganesh@ubuntu.ubuntu-domain>
2012-09-02 10:09:16 +0530

    quick navigation wizard fix.

--- app/assets/javascripts/items.js
+++ app/assets/javascripts/items.js
@@ -1,11 +1,11 @@
 //$(document).ready(function(){
-    $('div.Homepopupgraybox div.Boxtopblock span.Openclosebut a#wizardbutton').click(function() {
-    //$('div.Homepopupgraybox div.Boxtopblock span.Openclosebut a#wizardbutton').live('click', function(){
+    //$('span.Openclosebut a#wizardbutton').click(function() {
+    $('div.Homepopupgraybox div.Boxtopblock span.Openclosebut a#wizardbutton').live('click', function(){
         $("#wizardbutton").toggleClass('Close Open');
         $(this).text(function(i,text) {
             return_text =  (text == 'Close') ? 'Open' : 'Close';
         });
-       $("div.Homepopupgraybox div.Boxtopblock span.Openclosebut a#wizardbutton").text(return_text);   
+       $("span.Openclosebut a#wizardbutton").text(return_text);   
         $("div.Grayboxmid").toggle();
         return false;
     })
