70d393c0f12f5b6dab9f49fcdcb3df5cfd07b301
Saravana79 <saravanak@gmail.com>
2012-09-09 22:16:58 +0530

    Try - with different terms for search

--- app/controllers/contents_controller.rb
+++ app/controllers/contents_controller.rb
@@ -193,9 +193,9 @@ def filter
     page_no  = params[:page_no].present? ? params[:page_no] :2
     results = Sunspot.more_like_this(@content) do
       fields :title      
-      boost_by_relevance true
-      minimum_term_frequency frequency
-      minimum_word_length 2
+      #boost_by_relevance true
+      #minimum_term_frequency frequency
+      #minimum_word_length 2
       paginate(:page => page_no, :per_page => per_page)
     end
     @related_contents = results.results
