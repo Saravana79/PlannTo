86b730af6643112831d18b1d96d81466ed1e5c7a
Shanmukha <shanmukhakatta@gmail.com>
2012-09-21 17:20:56 +0530

    sitemap

--- app/controllers/sitemap_controller.rb
+++ app/controllers/sitemap_controller.rb
@@ -3,7 +3,9 @@ class SitemapController < ApplicationController
   def index
   	headers['Content-Type'] = 'application/xml'
   	base_url = "http://#{request.host_with_port}"
-   	static_urls = [ {:url => base_url +'/car/search',      :updated_at => "", :priority => 0.8},
+   @pages_to_visit1 =[]
+  	if params[:static] == "true"
+  	    static_urls = [ {:url => base_url +'/car/search',      :updated_at => "", :priority => 0.8},
    					{:url => base_url +'/bike/search',      :updated_at => "", :priority => 0.8},
    					{:url => base_url +'/cycle/search',      :updated_at => "", :priority => 0.8},
    					{:url => base_url +'/camera/search',      :updated_at => "", :priority => 0.8},
@@ -18,10 +20,16 @@ class SitemapController < ApplicationController
    					{:url => base_url +'/cameras/guides/Beginner%20Guide',      :updated_at => "", :priority => 0.8},
    					{:url => base_url +'/cycles/guides/Beginner%20Guide',      :updated_at => "", :priority => 0.8}				
    				 ]
-   @pages_to_visit1  = static_urls
-   @pages_to_visit1+= Item.all.collect{ |item| {:url =>  base_url + item.get_url() ,  :updated_at => I18n.l(item.updated_at.nil? ? Time.now : item.updated_at, :format => :w3c), :priority => 0.8 }}
-   @pages_to_visit1+= Content.all.collect{  |content| {:url =>  base_url + "/contents/" + content.id.to_s,  :updated_at => I18n.l(content.updated_at.nil?? Time.now : content.updated_at , :format => :w3c)} }
-    
+      @pages_to_visit1+= static_urls
+   end
+   if params[:object_type] and params[:itemtype]
+       item_type = Itemtype.find_by_itemtype(params[:itemtype])
+      if params[:object_type] == "item"
+         @pages_to_visit1+= item_type.items.collect{ |item| {:url =>  base_url + item.get_url() ,  :updated_at => I18n.l(item.updated_at.nil? ? Time.now : item.updated_at, :format => :w3c), :priority => 0.8 }}
+      elsif params[:object_type] == "content"  
+        @pages_to_visit1+= item_type.contents.collect{  |content| {:url =>  base_url + "/contents/" + content.id.to_s,  :updated_at => I18n.l(content.updated_at.nil?? Time.now : content.updated_at , :format => :w3c)} }
+     end 
+   end  
     respond_to do |format|
       format.xml {   @pages_to_visit=@pages_to_visit1}
     end
