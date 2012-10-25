d6ba265d8e633bfa4680a44f1a2beff6d64c360a
Saravana79 <saravanak@gmail.com>
2012-10-25 13:09:38 +0530

    Text is changed in activity and also about us text is modified.

--- app/helpers/contents_helper.rb
+++ app/helpers/contents_helper.rb
@@ -220,4 +220,7 @@ module ContentsHelper
     end
     
   end
+  def indefinite_articlerize(params_word)
+    %w(a e i o u).include?(params_word[0].downcase) ? "an #{params_word}" : "a #{params_word}"
+  end
 end
