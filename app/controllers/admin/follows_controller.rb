91d33bb0f45f6fb654d1e9c34433a817cdaa6767
Shanmukha <shanmukhakatta@gmail.com>
2012-11-16 18:24:56 +0530

    Admin section

--- app/controllers/admin/follows_controller.rb
+++ app/controllers/admin/follows_controller.rb
@@ -3,7 +3,7 @@ class Admin::FollowsController < ApplicationController
    before_filter :authenticate_admin_user!
   def index
     @no_follows = Follow.find(:all).size
-    @follows = Follow.order(:created_at).paginate(:per_page => 20,:page => params[:page])
+    @follows = Follow.order('created_at desc').paginate(:per_page => 20,:page => params[:page])
   end
 
 end
--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -3,7 +3,7 @@ class Admin::UsersController < ApplicationController
    before_filter :authenticate_admin_user!
   def index
     @no_users = User.find(:all).size
-    @users = User.order(:created_at).paginate(:per_page => 20,:page => params[:page])
+    @users = User.order('created_at desc').paginate(:per_page => 20,:page => params[:page])
   end
 
 end
