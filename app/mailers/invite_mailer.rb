d694f545600d2172826c5129152562ac4b0e87fc
Saravana79 <saravanak@gmail.com>
2012-11-19 13:03:03 +0530

    Added the name of the email id in the msg.

--- app/mailers/invite_mailer.rb
+++ app/mailers/invite_mailer.rb
@@ -3,7 +3,7 @@ class InviteMailer < ActionMailer::Base
  
   def invite_friend(invitation)
     @invitation = invitation
-    subject = "#{invitation.sender.name} invited you as #{@invitation.follow_type.humanize}"
+    subject = "#{invitation.sender.name} invited you as #{@invitation.follow_type.humanize} of #{@invitation.item.name}"
     mail(:to => invitation.email, :subject => subject)
   end
  
