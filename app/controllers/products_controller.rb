ea98bfbc63db9894b4b7573ec40052817251c836
Shanmukha <shanmukhakatta@gmail.com>
2012-12-15 13:42:47 +0530

    google search popup

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -11,12 +11,15 @@ class ProductsController < ApplicationController
   
   def set_referer
     @item = Item.find(params[:id])
+    
    if @item.name == "Vibgyor Persona 200"
-     if request.referer.nil?
+    unless request.referer.nil?
+     if request.referer.include?("google")     
        session[:http_referer] = request.referer
        session[:referer_counter] = 1
        return true
      end
+   end
    end 
      if !session[:referer_counter].nil?  &&  request.env["HTTP_X_REQUESTED_WITH"] != "XMLHttpRequest"
        session[:referer_counter]+= 1
