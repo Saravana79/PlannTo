8d821593618de29ce25794927de9adc15599a71b
Shanmukha <shanmukhakatta@gmail.com>
2012-12-17 20:43:27 +0530

    Recommendations

--- app/models/buying_plan.rb
+++ app/models/buying_plan.rb
@@ -12,7 +12,18 @@ class BuyingPlan < ActiveRecord::Base
   def get_first_item
     Item.find(Follow.for_follower(self.user).where(:followable_type =>  self.itemtype.itemtype, :follow_type =>Follow::ProductFollowType::Buyer).first.followable_id) rescue "" 
  end
- 
+  
+  def self.recommended_items(answers)
+    @items = []
+    answers.each do |ans|
+      ans.recommendations.each  do |reco|
+      
+        @items << reco.item
+    end
+    end
+    return @items
+  end 
+  
   def content_vote_count
     count = $redis.get("#{VoteCount::REDIS_BUYING_PLAN_VOTE_KEY_PREFIX}#{self.id}")
     if count.nil?
