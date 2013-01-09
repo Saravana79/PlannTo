7cf958d50d5825c2df500d6d3ceebaa466110364
Shanmukha <shanmukhakatta@gmail.com>
2013-01-09 12:40:39 +0530

    Preferences

--- app/controllers/preferences_controller.rb
+++ app/controllers/preferences_controller.rb
@@ -18,9 +18,10 @@ class PreferencesController < ApplicationController
     @itemtype = @buying_plan.itemtype
     @follow_types = Itemtype.get_followable_types(@buying_plan.itemtype.itemtype)
     @follow_item = Follow.for_follower(@buying_plan.user).where(:followable_type => @follow_types, :follow_type =>Follow::ProductFollowType::Buyer).group_by(&:followable_type)
+    @item_ids = []
     if @follow_item.size >0
       @item_ids = @follow_item[@itemtype.itemtype].collect(&:followable_id).join(",") 
-      @where_to_buy_items = Itemdetail.where("itemid in (?) and status = 1 and isError = 0", @item_ids.split(",")).includes(:vendor).group('vendors.name').order(:price)
+      @where_to_buy_items = Itemdetail.where("itemid in (?) and status = 1 and isError = 0", @item_ids.split(",")).includes(:vendor).order(:price)
     end
     if params[:type] == "guides"
       @item_ids = ""
