96dca8fd2b55bd64f2b9616d4dc6f7357e1db753
Saravana79 <saravanak@gmail.com>
2013-02-14 18:35:46 +0530

    Corrected the status issue.

--- app/controllers/article_contents_controller.rb
+++ app/controllers/article_contents_controller.rb
@@ -39,7 +39,7 @@ class ArticleContentsController < ApplicationController
     Point.add_point_system(current_user, @article, Point::PointReason::CONTENT_SHARE) unless @article.errors.any?
    UserActivity.save_user_activity(current_user,@article.id,"created",@article.sub_type,@article.id,request.remote_ip)  if @article.id!=nil
    session[:content_id] = @article.id
-   if current_user.total_points > 10
+   if current_user.total_points < 10
      @article.update_attribute('status',2) 
      @display = 'false'
    end 
