8d12f36237e719eb9cf74ecf0fea1713ff4de681
Shanmukha <shanmukhakatta@gmail.com>
2013-04-28 15:01:41 +0530

    Latest changes

---
+++ db/migrate/20130428084907_article_domain_for_exist_data.rb
@@ -0,0 +1,13 @@
+class ArticleDomainForExistData < ActiveRecord::Migration
+  def up
+     ArticleContent.where('url is not null').each do |article|
+       url = "http://#{article.url}" if URI.parse(article.url).scheme.nil?
+       host = URI.parse(article.url).host.downcase rescue ""
+       article.update_attribute('domain',(host.start_with?('www.') ? host[4..-1] : host rescue ""))
+      
+      end
+  end
+
+  def down
+  end
+end
