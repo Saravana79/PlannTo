8ad8c3195275ab9a57cf671ec424b6de56ad3996
Saravana79 <saravanak@gmail.com>
2013-05-02 13:04:45 +0530

    Migration is done only for data over 11000.

--- db/migrate/20130428084907_article_domain_for_exist_data.rb
+++ db/migrate/20130428084907_article_domain_for_exist_data.rb
@@ -1,6 +1,6 @@
 class ArticleDomainForExistData < ActiveRecord::Migration
   def up
-     ArticleContent.where('url is not null').each do |article|
+     ArticleContent.where('url is not null and id > 11000').each do |article|
        url = "http://#{article.url}" if URI.parse(article.url).scheme.nil?
        host = URI.parse(article.url).host.downcase rescue ""
        article.update_attribute('domain',(host.start_with?('www.') ? host[4..-1] : host rescue ""))
