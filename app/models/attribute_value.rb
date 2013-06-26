2a882d4b210ebd58d43fbf1c28321cacf43924bc
Tripurari <tripurari.singh@indusnet.co.in>
2013-06-26 23:41:59 +0530

    reverted the code join from previous one for item specification summmary list

--- app/models/attribute_value.rb
+++ app/models/attribute_value.rb
@@ -6,4 +6,37 @@ class AttributeValue < ActiveRecord::Base
   belongs_to :user, :foreign_key => 'created_by'
 
 
+  def proorcon
+    key = "nothing"
+    self.attribute.item_specification_summary_lists.each do |a|
+      unless(self.value.nil? or self.value.strip.empty?)
+        case a.condition
+        when "Not Equal"
+          if(a.value1 != "****")
+             key = {key: a.proorcon, description: a.description, title: a.title} if self.value.downcase.strip != a.value1.downcase.strip
+          end
+        when "Lesser"
+          key = {key: a.proorcon, description: a.description , title: a.title} if self.value.to_f < a.value1.to_f
+        when "Greater"
+          key = {key: a.proorcon, description: a.description, title: a.title} if self.value.to_f > a.value1.to_f
+        when "Equal"
+          if(a.value1 == "****")            
+              key = {key: a.proorcon, description: a.description, title: a.title}       
+          else
+             key = {key: a.proorcon, description: a.description, title: a.title} if self.value.downcase.strip == a.value1.downcase.strip          
+          end
+        when "Contains"
+             key = {key: a.proorcon, description: a.description, title: a.title} if self.value.downcase.include? a.value1.downcase.strip                    
+        when "Between"
+          key = {key: a.proorcon, description: a.description, title: a.title} if self.value.to_f > a.value1.to_f && self.value.to_f < a.value2.to_f
+        
+        end
+      else
+        if(a.condition == "Not Equal" and a.value1 == "****") 
+            key = {key: a.proorcon, description: a.description, title: a.title}
+        end
+      end
+    end
+    key
+  end 
 end
--- app/models/item_specification_summary_list.rb
+++ app/models/item_specification_summary_list.rb
@@ -4,41 +4,4 @@ class ItemSpecificationSummaryList < ActiveRecord::Base
 
 	attr_protected :sortorder
 
-  def procon
-  	key = "nothing"
-  	logger.info "======================#{self.attributes}"
-  	# self.attribute.attribute_values.find_by_item_id()
-      unless(self['value'].nil? or self['value'].strip.empty?)
-    	case condition
-    	when "Not Equal"
-          if(value1 != "****")
-  	  		   key = {key: proorcon, description: description, title: title} if self['value'].downcase.strip != value1.downcase.strip
-          end
-          key
-  	  	when "Lesser"
-  	  		key = {key: proorcon, description: description , title: title} if self['value'].to_f < value1.to_f
-  	  	when "Greater"
-  	  		key = {key: proorcon, description: description, title: title} if self['value'].to_f > value1.to_f
-  	  	when "Equal"
-          if(value1 == "****")            
-              key = {key: proorcon, description: description, title: title}       
-          else
-             key = {key: proorcon, description: description, title: title} if self['value'].downcase.strip == value1.downcase.strip          
-          end
-        when "Contains"
-             key = {key: proorcon, description: description, title: title} if self['value'].downcase.include? value1.downcase.strip                    
-  	  	when "Between"
-  	  		key = {key: proorcon, description: description, title: title} if self['value'].to_f < value1.to_f && self['value'].to_f > value2.to_f
-  	  	
-    		end
-      else
-        if(condition == "Not Equal" and value1 == "****") 
-            key = {key: proorcon, description: description, title: title}
-        end
-        key
-  		end
-  		logger.info "======================#{key}"
-  		key
-  	end
-  	
-  end
+end
