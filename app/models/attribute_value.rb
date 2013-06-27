f5802b7091309baff8892c216d220d59465c57fa
Saravana79 <saravanak@gmail.com>
2013-06-27 15:53:12 +0530

    Fixed the compare page for resolution.

--- app/models/attribute_value.rb
+++ app/models/attribute_value.rb
@@ -8,27 +8,28 @@ class AttributeValue < ActiveRecord::Base
 
   def proorcon
     key = "nothing"
+    tempValue = getValue
     self.attribute.item_specification_summary_lists.each do |a|
-      unless(self.value.nil? or self.value.strip.empty?)
+      unless(tempValue.nil? or tempValue.strip.empty?)
         case a.condition
         when "Not Equal"
           if(a.value1 != "****")
-             key = {key: a.proorcon, description: a.description, title: a.title} if self.value.downcase.strip != a.value1.downcase.strip
+             key = {key: a.proorcon, description: a.description, title: a.title} if tempValue.downcase.strip != a.value1.downcase.strip
           end
         when "Lesser"
-          key = {key: a.proorcon, description: a.description , title: a.title} if self.value.to_f < a.value1.to_f
+          key = {key: a.proorcon, description: a.description , title: a.title} if tempValue.to_f < a.value1.to_f
         when "Greater"
-          key = {key: a.proorcon, description: a.description, title: a.title} if self.value.to_f > a.value1.to_f
+          key = {key: a.proorcon, description: a.description, title: a.title} if tempValue.to_f > a.value1.to_f
         when "Equal"
           if(a.value1 == "****")            
               key = {key: a.proorcon, description: a.description, title: a.title}       
           else
-             key = {key: a.proorcon, description: a.description, title: a.title} if self.value.downcase.strip == a.value1.downcase.strip          
+             key = {key: a.proorcon, description: a.description, title: a.title} if tempValue.downcase.strip == a.value1.downcase.strip          
           end
         when "Contains"
-             key = {key: a.proorcon, description: a.description, title: a.title} if self.value.downcase.include? a.value1.downcase.strip                    
+             key = {key: a.proorcon, description: a.description, title: a.title} if tempValue.downcase.include? a.value1.downcase.strip                    
         when "Between"
-          key = {key: a.proorcon, description: a.description, title: a.title} if self.value.to_f > a.value1.to_f && self.value.to_f < a.value2.to_f
+          key = {key: a.proorcon, description: a.description, title: a.title} if tempValue.to_f > a.value1.to_f && tempValue.to_f < a.value2.to_f
         
         end
       else
@@ -39,4 +40,34 @@ class AttributeValue < ActiveRecord::Base
     end
     key
   end 
+
+  def groupbyvalue
+
+    tempValue = getValue
+    
+  end 
+
+  def getValue
+    if(self.attribute_id == 170)
+       if(self.value.include? "/")
+         self.value.split("/")[1]
+       else
+        self.value
+       end
+    elsif(self.attribute_id == 116)
+       if(self.value.include? "x")
+         self.value.split("/")[0]
+       else
+        self.value
+       end
+    elsif(self.attribute_id == 328)
+       if(self.value.include? "@")
+         self.value.split("/")[0]
+       else
+        self.value
+       end
+    else
+      self.value
+    end
+  end
 end
