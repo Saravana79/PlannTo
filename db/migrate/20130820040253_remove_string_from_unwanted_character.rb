0912c478f3442c934c2e9b1387f67efa3e198a4d
Shanmukha <shanmukhakatta@gmail.com>
2013-08-20 10:06:10 +0530

    migration for remove string after | in title

---
+++ db/migrate/20130820040253_remove_string_from_unwanted_character.rb
@@ -0,0 +1,21 @@
+class RemoveStringFromUnwantedCharacter < ActiveRecord::Migration
+  def up
+    ArticleContent.where("url is not null").each do |article_content|
+      unless article_content.url.nil?
+        if article_content.title.include?("|")
+          title = article_content.title.slice(0..(article_content.title.index('|'))).gsub(/\|/, "")
+          article_content.update_attribute("title",title)
+        end
+        if article_content.title.include?("~")
+          title = article_content.title.slice(0..(article_content.title.index('|'))).gsub(/\~/, "")
+          article_content.update_attribute("title",title)
+        end
+      end     
+    end
+  end
+
+  def down
+  end
+end
+
+ 
