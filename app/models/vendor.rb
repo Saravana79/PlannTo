0f8ce705b9ea8cf5609592b3394289367240eb19
Shanmukha <shanmukhakatta@gmail.com>
2013-08-30 09:07:47 +0530

    seller ratings

--- app/models/vendor.rb
+++ app/models/vendor.rb
@@ -1,2 +1,11 @@
 class Vendor < ActiveRecord::Base
+
+ def rating
+   ItemRating.where(:item_id => self.id).first.average_rating rescue 0.0 
+ end
+ 
+ def self.get_item_object(item)
+    vendor = Vendor.find(item.site)
+    Item.where(:id => vendor.item_id).first rescue ""
+ end
 end
