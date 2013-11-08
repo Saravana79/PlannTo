902063f569a461d10568a1025436b55fed441d04
Shanmukha <shanmukhakatta@gmail.com>
2013-11-08 15:24:22 +0530

    impression report changes

--- app/controllers/admin/impression_reports_controller.rb
+++ app/controllers/admin/impression_reports_controller.rb
@@ -7,9 +7,8 @@ class Admin::ImpressionReportsController < ApplicationController
    @publisher = Publisher.find(publisher_id)
    @start_date = params[:from_date].blank? ? 1.month.ago : params[:from_date]
    @end_date = params[:to_date].blank? ? Time.now : params[:to_date]
-
-   @impressions = AddImpression.where("publisher_id=? and DATE(impression_time) >=? and DATE(impression_time) <=?",publisher_id,@start_date.to_date,@end_date.to_date).order('created_at desc').paginate(:per_page => 10,:page => params[:page])
-   @impressionscount  = @impressions.count
+   @impressions = AddImpression.paginate_by_sql("select impression_time,hosted_site_url,count(*) as count from add_impressions where publisher_id=#{publisher_id} and impression_time >='#{@start_date.to_date}' and  DATE(impression_time)<='#{@end_date.to_date}' group by hosted_site_url order by count(*) desc",:per_page => 10,:page => params[:page])
+    @impressionscount  = AddImpression.where("publisher_id=? and DATE(impression_time) >=? and DATE(impression_time) <=?",publisher_id,@start_date.to_date,@end_date.to_date).order('created_at desc').count
   @clicks = Click.where("publisher_id=? and DATE(timestamp) >=? and DATE(timestamp) <=?",publisher_id,@start_date.to_date,@end_date.to_date).order('created_at desc').paginate(:per_page => 10,:page => params[:page])
    @clickscount = @clicks.count
    @total_orders =  OrderHistory.where("publisher_id=? and DATE(order_date) >=? and DATE(order_date) <=?",publisher_id,@start_date.to_date,@end_date.to_date).sum('no_of_orders')
