295360e7683e419993b2d6dfb1a4ea3014d5dd11
Saravana79 <saravanak@gmail.com>
2013-11-25 11:35:54 +0530

    missed this file.

--- app/models/click.rb
+++ app/models/click.rb
@@ -1,13 +1,14 @@
 class Click < ActiveRecord::Base
   belongs_to :item
   belongs_to :user
-  def self.save_click_data(url,request_referer,time,item_id,user,remote_ip,impression_id,publisher)
+  def self.save_click_data(url,request_referer,time,item_id,user,remote_ip,impression_id,publisher,vendor_id)
     click = Click.new
     click.impression_id = impression_id
     click.click_url = url
     click.hosted_site_url = request_referer
     click.timestamp = time
     click.item_id = item_id
+    click.vendor_id = vendor_id
     unless user.nil?
       click.user_id = user.id
     end
