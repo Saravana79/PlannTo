57502d86fcb26a2ef8a507171ce832ec5aee977d
Tripurari <tripurari.singh@indusnet.co.in>
2013-12-12 00:23:06 +0530

    multiple file upload and file dimension validation in advertisement creation

--- app/models/advertisement.rb
+++ app/models/advertisement.rb
@@ -8,5 +8,18 @@ class Advertisement < ActiveRecord::Base
                      :s3_credentials => "config/s3.yml",
                      :path => "images/advertisements/:id/:style/:filename",
                      :url  => ":s3_sg_url"
-   belongs_to :content                  
+   belongs_to :content
+
+   validate :file_dimensions 
+
+   private
+
+   def file_dimensions
+         hieght_width = self.ad_size.split("*")
+         dimensions = Paperclip::Geometry.from_file(upload_image.queued_for_write[:original].path)
+         logger.info "===========================================#{dimensions.width} +++++++++++++++#{dimensions.height}"
+         if dimensions.width.to_i.to_s != hieght_width[0] || dimensions.height.to_i.to_s != hieght_width[1]
+            errors.add :file, "Width must be #{hieght_width[0]}px and height must be #{hieght_width[1]}px"
+         end
+  end                 
 end
