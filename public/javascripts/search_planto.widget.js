ac2d4136e1d2a95218592c4e91067620c59dbb60
Saravana79 <saravanak@gmail.com>
2013-12-18 00:09:52 +0530

    fixed the jqyer ui loading issue.

--- public/javascripts/search_planto.widget.js
+++ public/javascripts/search_planto.widget.js
@@ -10,7 +10,7 @@ var PlannTo = (function (window, undefined) {
     //for production
     var domain = "www.plannto.com";
     //for development
-//     var domain = "localhost:3000";
+  //   var domain = "localhost:3000";
     // Localize jQuery variable
     var jQuery;
 
@@ -20,34 +20,20 @@ var PlannTo = (function (window, undefined) {
         script_tag.setAttribute("type", "text/javascript");
         script_tag.setAttribute("src",
             "http://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js");
-
-
+        
         if (script_tag.readyState) {
             script_tag.onreadystatechange = function () { // For old versions of IE
                 if (this.readyState == 'complete' || this.readyState == 'loaded') {
-                    scriptLoadHandler();
-                }
+                   scriptLoadHandler();
+                            }
             };
         } else { // Other browsers
             script_tag.onload = scriptLoadHandler;
+            
         }
         // Try to find the head, otherwise default to the documentElement
         (document.getElementsByTagName("head")[0] || document.documentElement).appendChild(script_tag);
-        var script_tag = document.createElement('script');
-        script_tag.setAttribute("type", "text/javascript");
-        script_tag.setAttribute("src",
-            "http://ajax.googleapis.com/ajax/libs/jqueryui/1.8.23/jquery-ui.min.js");
-        if (script_tag.readyState) {
-            script_tag.onreadystatechange = function () { // For old versions of IE
-                if (this.readyState == 'complete' || this.readyState == 'loaded') {
-                    scriptLoadHandler();
-                }
-            };
-        } else { // Other browsers
-            script_tag.onload = scriptLoadHandler;
-        }
-
-        (document.getElementsByTagName("head")[0] || document.documentElement).appendChild(script_tag);
+        
 
 
     } else {
@@ -63,13 +49,20 @@ var PlannTo = (function (window, undefined) {
     function scriptLoadHandler() {
         // Restore jQuery and window.jQuery to their previous values and store the
         // new jQuery in our local jQuery variable
-        jQuery = window.jQuery.noConflict(true);
+       jQuery = window.jQuery.noConflict();
         PlannTo.jQuery = jQuery;
         // Call our main function
-
+        if (window.jQuery.ui === undefined)
+        {
+            jQuery.getScript('http://ajax.googleapis.com/ajax/libs/jqueryui/1.8.6/jquery-ui.min.js', function() {
+                     jQuery.noConflict(true);
+            });
+        }
         main();
+        
     }
 
+  
     function getParam(url, sname) {
         var a = document.createElement('a');
         a.href = url;
