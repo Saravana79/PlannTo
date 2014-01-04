022d3740dba2496ed5390431d3f24ffe69d1caa7
Saravana Kumar <saravanak@gmail.com>
2014-01-04 15:46:49 +0530

    removed the local host references

--- config/application.rb
+++ config/application.rb
@@ -8,6 +8,7 @@ if defined?(Bundler)
   # If you want your assets lazily compiled in production, use this line
   # Bundler.require(:default, :assets, Rails.env)
 end
+
 configatron.configure_from_yaml(File.expand_path("../config.yml",__FILE__), :hash => Rails.env)
 module PlanNto
   class Application < Rails::Application
