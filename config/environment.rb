62e5d741f42d6dd37fe09c0017bea55ffa3b2861
Sivakumar <siva@plannto.com>
2014-04-02 14:49:19 +0530

    mailer settings changes

--- config/environment.rb
+++ config/environment.rb
@@ -2,14 +2,14 @@
 require File.expand_path('../application', __FILE__)
 require 'rails_sql_views'
 ActionMailer::Base.smtp_settings = {
-:address => "smtpout.asia.secureserver.net",
-:port => 80,
-:domain => "plannto.com",
-:user_name => "plannto.test@gmail.com",
-:password => "plannto@6",
-:authentication => :plain,
-:enable_starttls_auto => false,
-:openssl_verify_mode => 'none'
+    :address => "smtp.gmail.com",
+    :port => 25,
+    :domain => "plannto.com",
+    :user_name => "plannto.test@gmail.com",
+    :password => "plannto@6",
+    :authentication => :plain,
+    :enable_starttls_auto => true,
+    :openssl_verify_mode => 'none'
 }
 require 'aws/s3'
 AWS::S3::DEFAULT_HOST = "s3-ap-southeast-1.amazonaws.com" # if using sg buckets
