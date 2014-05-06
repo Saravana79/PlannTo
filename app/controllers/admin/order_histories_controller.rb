95963d790673939f3092f940c2962c3d35fee250
Sivakumar <siva@plannto.com>
2014-05-06 10:21:43 +0530

    changes for impression count

--- app/controllers/admin/order_histories_controller.rb
+++ app/controllers/admin/order_histories_controller.rb
@@ -3,6 +3,22 @@ class Admin::OrderHistoriesController < ApplicationController
  layout "product"
 
   def index
+
+    # condition = "1=1"
+    condition = ""
+    unless params[:search].blank?
+      condition = condition + " and vendor_ids = #{params[:search][:vendor_id]}" unless params[:search][:vendor_id].blank?
+      condition = condition + " and order_status = '#{params[:search][:order_status]}'" unless params[:search][:order_status].blank?
+      condition = condition + " and payment_status = '#{params[:search][:payment_status]}'" unless params[:search][:payment_status].blank?
+    end
+
+    if params[:commit] == "Clear"
+      condition = ""
+      params[:search] = {}
+    elsif params[:commit] != "Filter"
+      params[:search] ||= {}
+    end
+
     @impressionscount = 0
     @clickscount = 0
     publisher_id = UserRelationship.where(:relationship_type => "Publisher",:user_id => current_user.id).first.relationship_id
@@ -15,8 +31,11 @@ class Admin::OrderHistoriesController < ApplicationController
    # @clickscount = @clicks.count
    @total_orders =  OrderHistory.where("publisher_id=? and DATE(order_date) >=? and DATE(order_date) <=?",publisher_id,@start_date.to_date,@end_date.to_date).sum('no_of_orders')
    @total_revenue = OrderHistory.where("publisher_id=? and DATE(order_date) >=? and DATE(order_date) <=?",publisher_id,@start_date.to_date,@end_date.to_date).sum('total_revenue')
-   @order_histories = OrderHistory.where("publisher_id=? and DATE(order_date) >=? and DATE(order_date) <=?",publisher_id,@start_date.to_date,@end_date.to_date).order('order_date desc').paginate(:per_page => 20,:page => params[:page])
-   @vendors =  VendorDetail.all
+   @order_histories = OrderHistory.where("publisher_id=? and DATE(order_date) >=? and DATE(order_date) <=? #{condition}",publisher_id,@start_date.to_date,@end_date.to_date).order('order_date desc').paginate(:per_page => 20,:page => params[:page])
+
+   vendor_ids = @order_histories.map(&:vendor_ids)
+
+   @vendors =  Vendor.where(:id => vendor_ids.uniq)
 
     @result = AggregatedDetail.get_counts(@start_date.to_date, @end_date.to_date, publisher_id).first
     unless @result.blank?
--- app/models/order_history.rb
+++ app/models/order_history.rb
@@ -1,2 +1,3 @@
 class OrderHistory < ActiveRecord::Base
+
 end
