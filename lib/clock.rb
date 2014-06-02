1420e9cff31fdcdb0a7aafb7b6486a7da5e5d02f
Sivakumar <siva@plannto.com>
2014-06-02 19:06:03 +0530

    changes for resque process changed to scheduler

--- lib/clock.rb
+++ lib/clock.rb
@@ -16,53 +16,50 @@ module Clockwork
 
   #every(1.minutes, 'process_feeds')
 
-  every(1.day, 'Queeing Feed Process', :at => '00:00', :tz => "Asia/Kolkata") do
-    puts "Running Feed Process, at #{Time.zone.now}"
-      Resque.enqueue(FeedProcess, "process_feeds", Time.zone.now)
-  end
-
-  every(1.day, 'Queeing Advertisement ecpm calculation', :at => '02:00', :tz => "Asia/Kolkata") do
-    puts "Running Advertisement ecpm calculation, at #{Time.zone.now}"
-    Resque.enqueue(AdvertisementProcess, "calculate_ecpm", Time.zone.now)
-  end
-
-  every(1.day, 'Queeing Redis Update for RelatedItem', :at => '03:00', :tz => "Asia/Kolkata") do
-    puts "Running Redis Update for RelatedItem, at #{Time.zone.now}"
-    Resque.enqueue(RelatedItemUpdateProcess, Time.zone.now)
-  end
-
-  every(1.day, 'Queeing Redis Update for Item', :at => '05:00', :tz => "Asia/Kolkata") do
-    puts "Running Redis Update for Item, at #{Time.zone.now}"
-    Resque.enqueue(ItemUpdate, "update_item_details", Time.zone.now)
-  end
-
-  every(1.day, 'Queeing ItemAdDetail Update for Items', :at => '06:00', :tz => "Asia/Kolkata") do
-    puts "Running ItemAdDetail Update for Items, at #{Time.zone.now}"
-    Resque.enqueue(ItemAdDetailProcess, "update_ad_details_for_items", Time.zone.now, 1000)
-  end
-
-  every(1.hour, 'Queeing Aggregated Process') do
-    puts "Running Queeing Aggregated Process, at #{Time.zone.now}"
-    Resque.enqueue(AggregatedDetailProcess, Time.zone.now)
-  end
-
+  # TODO: will enable later
+  # every(1.day, 'Queeing Feed Process', :at => '00:00', :tz => "Asia/Kolkata") do
+  #   puts "Running Feed Process, at #{Time.zone.now}"
+  #     Resque.enqueue(FeedProcess, "process_feeds", Time.zone.now)
+  # end
+  #
+  # every(1.day, 'Queeing Advertisement ecpm calculation', :at => '02:00', :tz => "Asia/Kolkata") do
+  #   puts "Running Advertisement ecpm calculation, at #{Time.zone.now}"
+  #   Resque.enqueue(AdvertisementProcess, "calculate_ecpm", Time.zone.now)
+  # end
+  #
+  # every(1.day, 'Queeing Redis Update for RelatedItem', :at => '03:00', :tz => "Asia/Kolkata") do
+  #   puts "Running Redis Update for RelatedItem, at #{Time.zone.now}"
+  #   Resque.enqueue(RelatedItemUpdateProcess, Time.zone.now)
+  # end
+  #
+  # every(1.day, 'Queeing Redis Update for Item', :at => '05:00', :tz => "Asia/Kolkata") do
+  #   puts "Running Redis Update for Item, at #{Time.zone.now}"
+  #   Resque.enqueue(ItemUpdate, "update_item_details", Time.zone.now)
+  # end
+  #
+  # every(1.day, 'Queeing ItemAdDetail Update for Items', :at => '06:00', :tz => "Asia/Kolkata") do
+  #   puts "Running ItemAdDetail Update for Items, at #{Time.zone.now}"
+  #   Resque.enqueue(ItemAdDetailProcess, "update_ad_details_for_items", Time.zone.now, 1000)
+  # end
+  #
+  # every(1.hour, 'Queeing Aggregated Process') do
+  #   puts "Running Queeing Aggregated Process, at #{Time.zone.now}"
+  #   Resque.enqueue(AggregatedDetailProcess, Time.zone.now)
+  # end
+  #
   # every(2.hours, 'Queeing Feed Process') do
   #   puts "Running Priorities Feed Process, at #{Time.zone.now}"
   #   Resque.enqueue(FeedProcess, "process_feeds", Time.zone.now, nil, priorities=true)
   # end
-
-  every(1.day, 'Queeing ContentAdDetail Update for Items', :at => '07:00', :tz => "Asia/Kolkata") do
-    puts "Running ContentAdDetail Update for Items, at #{Time.zone.now}"
-    Resque.enqueue(ContentAdDetailProcess, "update_ad_details_for_contents", Time.zone.now, 1000)
-  end
-
-  every(6.hours, 'Queeing Missingurl Process from Redis') do
-    puts "Running Missingurl Process, at #{Time.zone.now}"
-    Resque.enqueue(MissingurlProcess, "update_by_missing_records", Time.zone.now)
-  end
-
-  #every(3.minutes, 'less.frequent.job')
-  #every(1.hour, 'hourly.job')
   #
-  #every(1.day, 'midnight.job', :at => '00:00')
+  # every(1.day, 'Queeing ContentAdDetail Update for Items', :at => '07:00', :tz => "Asia/Kolkata") do
+  #   puts "Running ContentAdDetail Update for Items, at #{Time.zone.now}"
+  #   Resque.enqueue(ContentAdDetailProcess, "update_ad_details_for_contents", Time.zone.now, 1000)
+  # end
+  #
+  # every(6.hours, 'Queeing Missingurl Process from Redis') do
+  #   puts "Running Missingurl Process, at #{Time.zone.now}"
+  #   Resque.enqueue(MissingurlProcess, "update_by_missing_records", Time.zone.now)
+  # end
+
 end
--- lib/tasks/scheduler.rake
+++ lib/tasks/scheduler.rake
@@ -4,3 +4,37 @@ task :feed_process => :environment do
   Resque.enqueue(FeedProcess, "process_feeds", Time.zone.now, nil, priorities=true)
 end
 
+task :advertisement_process => :environment do
+  puts "Running Advertisement ecpm calculation, at #{Time.zone.now}"
+  Resque.enqueue(AdvertisementProcess, "calculate_ecpm", Time.zone.now)
+end
+
+task :related_item_process => :environment do
+  puts "Running Redis Update for RelatedItem, at #{Time.zone.now}"
+  Resque.enqueue(RelatedItemUpdateProcess, Time.zone.now)
+end
+
+task :item_update => :environment do
+  puts "Running Redis Update for Item, at #{Time.zone.now}"
+  Resque.enqueue(ItemUpdate, "update_item_details", Time.zone.now)
+end
+
+task :item_ad_detail_process => :environment do
+  puts "Running ItemAdDetail Update for Items, at #{Time.zone.now}"
+  Resque.enqueue(ItemAdDetailProcess, "update_ad_details_for_items", Time.zone.now, 1000)
+end
+
+task :aggregated_detail_process => :environment do
+  puts "Running Queeing Aggregated Process, at #{Time.zone.now}"
+  Resque.enqueue(AggregatedDetailProcess, Time.zone.now)
+end
+
+task :content_ad_detail_process => :environment do
+  puts "Running ContentAdDetail Update for Items, at #{Time.zone.now}"
+  Resque.enqueue(ContentAdDetailProcess, "update_ad_details_for_contents", Time.zone.now, 1000)
+end
+
+task :missing_url_process => :environment do
+  puts "Running Missingurl Process, at #{Time.zone.now}"
+  Resque.enqueue(MissingurlProcess, "update_by_missing_records", Time.zone.now)
+end
