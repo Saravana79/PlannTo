f0d6ef771a8ca42d06f039ba3dae31bee829f7f4
Sivakumar <siva@plannto.com>
2014-06-17 18:13:22 +0530

    changes for error messages and show error quickly

--- app/controllers/error_messages_controller.rb
+++ app/controllers/error_messages_controller.rb
@@ -2,7 +2,7 @@ class ErrorMessagesController < ApplicationController
   before_filter :authenticate_admin_user!
   
   def index
-    @error_messages = ErrorMessage.order('created_at desc').all.paginate :page => params[:page],:per_page => 10
+    @error_messages = ErrorMessage.order('created_at desc').paginate :page => params[:page],:per_page => 10
   end
   def show
     @error_message = ErrorMessage.find(params[:id])
