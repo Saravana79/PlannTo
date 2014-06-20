797a3693543abf396f75de315e5e0128257624c1
Sivakumar <siva@plannto.com>
2014-06-20 18:17:19 +0530

    changes for mig

--- db/migrate/20140620113620_add_column_winning_price_and_enc_to_add_impressions.rb
+++ db/migrate/20140620113620_add_column_winning_price_and_enc_to_add_impressions.rb
@@ -1,6 +1,6 @@
 class AddColumnWinningPriceAndEncToAddImpressions < ActiveRecord::Migration
   def change
     add_column :add_impressions, :winning_price_enc, :text
-    add_column :add_impressions, :winning_price, :float
+    # add_column :add_impressions, :winning_price, :float
   end
 end
