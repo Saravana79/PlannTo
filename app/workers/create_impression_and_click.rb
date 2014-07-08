0cd01fb15a8ac9c67fa126d31287474955b5f5a5
Sivakumar <siva@plannto.com>
2014-07-08 18:03:10 +0530

    worker changes

--- app/workers/create_impression_and_click.rb
+++ app/workers/create_impression_and_click.rb
@@ -1,8 +1,15 @@
+require 'new_relic/agent/method_tracer'
 class CreateImpressionAndClick
   @queue = :create_impression_and_click
 
   def self.perform(class_name, obj_params)
     obj_params = JSON.parse(obj_params)
     class_name.constantize.send("create_new_record", obj_params)
+  end
+
+  class << self
+    include ::NewRelic::Agent::MethodTracer
+
+    add_method_tracer :generate_image, 'Custom/perform'
   end
 end
