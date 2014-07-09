e151cb879e926e608b0b95e93830349bd4968806
Sivakumar <siva@plannto.com>
2014-07-09 13:48:07 +0530

    changes for resque process

--- app/models/item.rb
+++ app/models/item.rb
@@ -1100,8 +1100,8 @@ end
     # query_to_get_price_and_vendor_ids = "select itemid as item_id,min(price) price,group_concat(distinct(site)) as vendor_id, i.itemtype_id as item_type, i.type as type from
     #                                     itemdetails id inner join items i on i.id = id.itemid where id.status in (1,3) and site in (9861,9882,9874,9880) group by itemid"
 
-    query_to_get_price_and_vendor_ids = "select itemid as item_id, min(price) as price, group_concat(site order by price) as vendor_id, group_concat(price order by price) as
-                                         pricestring from (select itemid, site, min(price)  as price from itemdetails where status in (1,3) and iserror != 1 and site in
+    query_to_get_price_and_vendor_ids = "select type, itemid as item_id, min(price) as price, group_concat(site order by price) as vendor_id, group_concat(price order by price) as
+                                         pricestring from (select itemid, site, min(price)  as price, type from itemdetails id inner join items i on id.itemid=i.id where id.status in (1,3) and iserror != 1 and site in
                                          (9861,9882,9874,9880)  group by itemid,site ) a group by itemid"
     # p_v_records = Item.find_by_sql(query_to_get_price_and_vendor_ids)
 
--- lib/tasks/resque.rake
+++ lib/tasks/resque.rake
@@ -2,10 +2,10 @@ require "resque/tasks"
 
 task "resque:setup" => :environment do
   ENV['QUEUE'] = '*'
-  Resque.after_fork do |job|
-    ActiveRecord::Base.establish_connection
-  end
-
+  # Resque.after_fork do |job|
+  #   ActiveRecord::Base.establish_connection
+  # end
+  Resque.before_fork = Proc.new { ActiveRecord::Base.establish_connection }
 end
 
 
