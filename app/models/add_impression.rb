e18f79f0de74446b180a9dcea2291f5ffccc911b
Sivakumar <siva@plannto.com>
2014-07-09 14:54:17 +0530

    changes for impression

--- app/models/add_impression.rb
+++ app/models/add_impression.rb
@@ -231,8 +231,8 @@ class AddImpression < ActiveRecord::Base
     impression_id = SecureRandom.uuid
     impression_params = {:imp_id => impression_id, :type => impression_type, :itemid => item_id, :request_referer => request_referer, :time => Time.zone.now, :user => user.blank? ? nil : user.id, :remote_ip => remote_ip, :impression_id => impressionid, :itemaccess => itemsaccess,
                          :params => url_params, :temp_user_id => plan_to_temp_user_id, :ad_id => ad_id, :winning_price => 0.0, :winning_price_enc => winning_price_enc}.to_json
-    # Resque.enqueue(CreateImpressionAndClick, 'AddImpression', impression_params)
-    AddImpression.create_new_record(impression_params)
+    Resque.enqueue(CreateImpressionAndClick, 'AddImpression', impression_params)
+    # AddImpression.create_new_record(impression_params)
     return impression_id
   end
 
