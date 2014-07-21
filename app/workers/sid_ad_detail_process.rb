4d481e5db3c5265423d3de71a142c6cf86a49ff9
Sivakumar <siva@plannto.com>
2014-07-21 18:19:58 +0530

    sid ad detail process

---
+++ app/workers/sid_ad_detail_process.rb
@@ -0,0 +1,22 @@
+class SidAdDetailProcess
+  @queue = :sid_ad_detail_process
+
+  def self.perform(method_name, actual_time, batch_size)
+    log = Logger.new 'log/sid_ad_detail_process.log'
+    #begin
+    log.debug "********** Start Processing SidAdDetail **********"
+    log.debug "********** Actual Time to Start #{actual_time.to_time.strftime('%b %d,%Y %r')} **********"
+
+    now_time = actual_time.to_time
+
+    SidAdDetail.send(method_name, log, batch_size, now_time)
+
+    #rescue Exception => e
+    #  log.debug "Have some problem while executing calculate ecpm, please find the error below"
+    #  log.debug e
+    #  NotificationMailer.resque_process_failure(e, e.backtrace, log, "Advertisement Process").deliver
+    #end
+    log.debug "********** End Processing SidAdDetail Update **********"
+    log.debug "\n"
+  end
+end
