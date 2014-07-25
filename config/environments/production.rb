71b6b4f2862ea00a97e13d614e340ad34f55a7ac
Sivakumar <siva@plannto.com>
2014-07-25 17:02:40 +0530

    assets path changes for cdn

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -48,7 +48,7 @@ PlanNto::Application.configure do
                      :password => ENV["MEMCACHIER_PASSWORD"]}
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server
-  config.action_controller.asset_host = "http://cdn1.plannto.com/static"
+  config.action_controller.asset_host = "cdn1.plannto.com/static"
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
   # config.assets.precompile += %w( search.js )
