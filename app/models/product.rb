1422f20e4963c33663fa3d9c759eaeb868173ce2
Sivakumar <siva@plannto.com>
2014-08-26 16:44:36 +0530

    changes for content

--- app/models/product.rb
+++ app/models/product.rb
@@ -18,9 +18,9 @@ has_one :manufacturer,
  def self.search_type(type)
    return [ "ItemtypeTag".camelize.constantize, "AttributeTag".camelize.constantize,"Topic".camelize.constantize,"Manufacturer".camelize.constantize, "CarGroup".camelize.constantize,"Mobile".camelize.constantize, "Tablet".camelize.constantize, "Car".camelize.constantize, "Camera".camelize.constantize,"Game".camelize.constantize,"GamingConsole".camelize.constantize,"Laptop".camelize.constantize,"Bike".camelize.constantize,"Cycle".camelize.constantize,"Tablet".camelize.constantize,"Hotel".camelize.constantize,"City".camelize.constantize] if (type.blank? || type.include?("Others"))
   if type.is_a?(Array)
-    return type.collect{|t| t.strip.camelize.singularize.constantize}
+    return type.collect{|t| t.to_s.gsub(/\s+/,'').strip.camelize.singularize.constantize}
   else
-    return type.strip.camelize.singularize.constantize
+    return type.to_s.gsub(/\s+/,'').strip.camelize.singularize.constantize
   end
  end
 
