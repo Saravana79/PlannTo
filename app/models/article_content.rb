28dd9731e94cc855f31aa2cec116c2a029480e40
Sivakumar <siva@plannto.com>
2014-09-20 14:24:41 +0530

    changes for article content

--- app/models/article_content.rb
+++ app/models/article_content.rb
@@ -339,7 +339,7 @@ class ArticleContent < Content
 
         param.merge!(:feed_url_id => new_feed_url.id, :default_item_id => "", :submit_url => "submit_url",
                      :article_content => { :itemtype_id => self.itemtype_id, :type => self.type, :thumbnail => self.thumbnail,
-                                           :title => self.title, :url => processed_url, :sub_type => self.sub_type, :description => self.description },
+                                           :title => self.title, :url => new_feed_url.url, :sub_type => self.sub_type, :description => self.description },
                      :share_from_home => "", :detail => "", :articles_item_id => article_item_ids, :external => "true", :score => "0", :old_default_values => nil,
                      :new_default_values => nil)
 
@@ -372,7 +372,7 @@ class ArticleContent < Content
 
         param.merge!(:feed_url_id => feed_url.blank? ? nil : feed_url.id, :default_item_id => "", :submit_url => "submit_url",
                      :article_content => { :itemtype_id => article_content.itemtype_id, :type => article_content.type, :thumbnail => article_content.thumbnail,
-                                           :title => article_content.title, :url => processed_url, :sub_type => article_content.sub_type, :description => article_content.description },
+                                           :title => article_content.title, :url => article_content.url, :sub_type => article_content.sub_type, :description => article_content.description },
                      :share_from_home => "", :detail => "", :articles_item_id => article_item_ids, :external => "true", :score => "0", :old_default_values => nil,
                      :new_default_values => nil)
 
