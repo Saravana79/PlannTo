6f6c593a4ce2a0a4223db41677254e05b24b4c46
Saravana Kumar <saravanak@gmail.com>
2014-09-20 12:07:38 +0530

    Modified the design of  600x120

--- app/models/feed.rb
+++ app/models/feed.rb
@@ -137,11 +137,11 @@ class Feed < ActiveRecord::Base
   def self.get_find_subtype(title)
     title_words = title.to_s.downcase #.split
 
-    tips = [' tip', ' trick']
+    tips = ['tip ', 'trick ']
     reviews = [' review', 'first impression', 'hands on', 'hands-on', 'first look', 'unboxing','handson']
-    comparisons = [' vs','versus','comparison','Competitor']
+    comparisons = [' vs ','versus','comparison','competitor']
     how_to = ["tutorial", "guide", "how to",'update','Wallpaper',' root',' reset']
-    lists = [" top", " best"]
+    lists = ["top ", "best "]
     photos = ["gallery",'photos','picture']
     news = ['launch', 'release', 'online', 'available', 'announce', 'official','upcoming','unveiled','leaks', ' Rs']
     specs = [' spec',' 3D']
