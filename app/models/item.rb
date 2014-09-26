87688b7ab14ae8104b1b880efbfa906f62e4a0e8
Sivakumar <siva@plannto.com>
2014-09-26 15:07:07 +0530

    changes for buying list process

--- app/models/item.rb
+++ app/models/item.rb
@@ -1403,117 +1403,121 @@ end
   end
 
   def self.buying_list_process_in_redis
-    length = $redis_rtb.llen("users:visits")
-    base_item_ids = Item.get_base_items_from_config()
-    source_categories = SourceCategory.get_source_category_with_paginations()
-    t_length = length
-    # start_point = 0
+    if $redis.get("buying_list_is_running").to_i == 0
+      $redis.set("buying_list_is_running", 1)
+      length = $redis_rtb.llen("users:visits")
+      base_item_ids = Item.get_base_items_from_config()
+      source_categories = SourceCategory.get_source_category_with_paginations()
+      t_length = length
+      # start_point = 0
 
-    begin
-      user_vals = $redis_rtb.lrange("users:visits", 0, 1000)
-      redis_rtb_hash = {}
+      begin
+        user_vals = $redis_rtb.lrange("users:visits", 0, 1000)
+        redis_rtb_hash = {}
 
-      user_vals.each do |each_user_val|
-        t_length-=1
-        p "Remaining Length each - #{t_length} - #{Time.now.strftime("%H:%M:%S")}"
-        unless each_user_val.blank?
-          user_id, url, type, item_ids, advertisement_id = each_user_val.split("<<")
-          already_exist = Item.check_if_already_exist_in_user_visits(source_categories, user_id, url)
-          ranking = 0
+        user_vals.each do |each_user_val|
+          t_length-=1
+          p "Remaining Length each - #{t_length} - #{Time.now.strftime("%H:%M:%S")}"
+          unless each_user_val.blank?
+            user_id, url, type, item_ids, advertisement_id = each_user_val.split("<<")
+            already_exist = Item.check_if_already_exist_in_user_visits(source_categories, user_id, url)
+            ranking = 0
 
-          if already_exist == false
-            case type
-              when "Reviews", "Spec", "Photo"
-                ranking = 10
-              when "Comparisons"
-                ranking = 5
-              when "Lists", "Others"
-                ranking = 2
-            end
-
-            item_ids = item_ids.to_s.split(",")
-            if item_ids.count < 10
-
-              u_key = "u:ac:#{user_id}"
-              u_values = $redis.hgetall(u_key)
-
-              # Remove expired items from user details
-              u_values.each do |key,val|
-                if key.include?("_la") && val.to_date < 2.weeks.ago.to_date
-                  item_id = key.gsub("_la", "")
-                  u_values.delete("#{item_id}_c")
-                  u_values.delete("#{item_id}_la")
-                end
+            if already_exist == false
+              case type
+                when "Reviews", "Spec", "Photo"
+                  ranking = 10
+                when "Comparisons"
+                  ranking = 5
+                when "Lists", "Others"
+                  ranking = 2
               end
 
-              # incrby count for items
-              item_ids.each do |each_key|
-                if u_values["#{each_key}_c"].blank?
-                  u_values["#{each_key}_c"] = ranking
-                  u_values["#{each_key}_la"] = Date.today.to_s
-                else
-                  old_ranking = u_values["#{each_key}_c"]
-                  u_values["#{each_key}_c"] = old_ranking.to_i + ranking
-                  u_values["#{each_key}_la"] = Date.today.to_s
+              item_ids = item_ids.to_s.split(",")
+              if item_ids.count < 10
+
+                u_key = "u:ac:#{user_id}"
+                u_values = $redis.hgetall(u_key)
+
+                # Remove expired items from user details
+                u_values.each do |key,val|
+                  if key.include?("_la") && val.to_date < 2.weeks.ago.to_date
+                    item_id = key.gsub("_la", "")
+                    u_values.delete("#{item_id}_c")
+                    u_values.delete("#{item_id}_la")
+                  end
                 end
-              end
 
-              proc_item_ids = u_values.map {|key,val| if key.include?("_c") && val.to_i > 30; key.gsub("_c",""); end}.compact
+                # incrby count for items
+                item_ids.each do |each_key|
+                  if u_values["#{each_key}_c"].blank?
+                    u_values["#{each_key}_c"] = ranking
+                    u_values["#{each_key}_la"] = Date.today.to_s
+                  else
+                    old_ranking = u_values["#{each_key}_c"]
+                    u_values["#{each_key}_c"] = old_ranking.to_i + ranking
+                    u_values["#{each_key}_la"] = Date.today.to_s
+                  end
+                end
 
-              unless proc_item_ids.blank?
-                buying_list = u_values["buyinglist"]
-                buying_list = buying_list.to_s.split(",")
+                proc_item_ids = u_values.map {|key,val| if key.include?("_c") && val.to_i > 30; key.gsub("_c",""); end}.compact
 
-                if !buying_list.blank?
-                  have_to_del = []
+                unless proc_item_ids.blank?
+                  buying_list = u_values["buyinglist"]
+                  buying_list = buying_list.to_s.split(",")
 
-                  #remove items from buyinglist which detail is expired
-                  buying_list.each do |each_item|
-                    if !u_values.include?("#{each_item}_c") || u_values["#{each_item}_c"].to_i < 30
-                      have_to_del << each_item
+                  if !buying_list.blank?
+                    have_to_del = []
+
+                    #remove items from buyinglist which detail is expired
+                    buying_list.each do |each_item|
+                      if !u_values.include?("#{each_item}_c") || u_values["#{each_item}_c"].to_i < 30
+                        have_to_del << each_item
+                      end
                     end
+
+                    buying_list = buying_list - have_to_del
+                    buying_list << proc_item_ids
+                    buying_list = buying_list.flatten.uniq
+                  else
+                    buying_list = proc_item_ids
                   end
 
-                  buying_list = buying_list - have_to_del
-                  buying_list << proc_item_ids
-                  buying_list = buying_list.flatten.uniq
+                  buying_list = buying_list.delete_if {|each_item| base_item_ids.include?(each_item)}
+
+                  u_values["buyinglist"] = buying_list.join(",")
+
+                  $redis.pipelined do
+                    $redis.hmset(u_key, u_values.to_a.flatten)
+                    $redis.expire(u_key, 2.weeks)
+                  end
+                  redis_rtb_hash.merge!("users:buyinglist:#{user_id}" => u_values["buyinglist"])
                 else
-                  buying_list = proc_item_ids
-                end
-
-                buying_list = buying_list.delete_if {|each_item| base_item_ids.include?(each_item)}
-
-                u_values["buyinglist"] = buying_list.join(",")
-
-                $redis.pipelined do
-                  $redis.hmset(u_key, u_values.to_a.flatten)
-                  $redis.expire(u_key, 2.weeks)
-                end
-                redis_rtb_hash.merge!("users:buyinglist:#{user_id}" => u_values["buyinglist"])
-              else
-                $redis.pipelined do
-                  $redis.hmset(u_key, u_values.to_a.flatten)
-                  $redis.expire(u_key, 2.weeks)
+                  $redis.pipelined do
+                    $redis.hmset(u_key, u_values.to_a.flatten)
+                    $redis.expire(u_key, 2.weeks)
+                  end
                 end
               end
             end
           end
         end
-      end
 
-      $redis_rtb.ltrim("users:visits", user_vals.count, -1) #TODO: temporary comment
+        $redis_rtb.ltrim("users:visits", user_vals.count, -1) #TODO: temporary comment
 
-      $redis_rtb.pipelined do
-        redis_rtb_hash.each do |key, val|
-          $redis_rtb.set(key, val)
-          $redis_rtb.expire(key, 2.weeks)
+        $redis_rtb.pipelined do
+          redis_rtb_hash.each do |key, val|
+            $redis_rtb.set(key, val)
+            $redis_rtb.expire(key, 2.weeks)
+          end
         end
-      end
 
-      length = length - 1001
-      # start_point = start_point + 1001
-      p "Remaining Length - #{length}"
-    end while length > 0
+        length = length - 1001
+        # start_point = start_point + 1001
+        p "Remaining Length - #{length}"
+      end while length > 0
+      $redis.set("buying_list_is_running", 0)
+    end
   end
 
   def self.check_if_already_exist_in_user_visits(source_categories, user_id, url)
