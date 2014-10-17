e362c6fcfbe2e1834e43297729231e7925806399
Sivakumar <siva@plannto.com>
2014-10-17 18:45:50 +0530

    custom require fixes

--- config/initializers/custom_require.rb
+++ config/initializers/custom_require.rb
@@ -1,6 +1,6 @@
 %w{custom_acts_as_follower authentication reputation ext/string addressable/uri}.each { |each_val| require each_val }
 
-if !Rails.env.production?
+if Rails.env.production?
   ENV['HEROKU_USER'] = "siva@plannto.com"
   ENV['HEROKU_PASSWORD'] = "sivaplannto"
   ENV["HEROKU_APP"] = "plannto"
