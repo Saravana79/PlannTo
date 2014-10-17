3d2353c1d123aee2c503143ed3d164c5c806791d
Sivakumar <siva@plannto.com>
2014-10-17 18:41:03 +0530

    config

--- config/initializers/custom_require.rb
+++ config/initializers/custom_require.rb
@@ -1,3 +1,9 @@
 %w{custom_acts_as_follower authentication reputation ext/string addressable/uri}.each { |each_val| require each_val }
 
-require "#{Rails.root}/lib/heroku_resque_auto_scale.rb"
+if !Rails.env.production?
+  ENV['HEROKU_USER'] = "siva@plannto.com"
+  ENV['HEROKU_PASSWORD'] = "sivaplannto"
+  ENV["HEROKU_APP"] = "plannto"
+
+  require "#{Rails.root}/lib/heroku_resque_auto_scale.rb"
+end
