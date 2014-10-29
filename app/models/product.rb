b3a1d155d357df20bc099138979cefeec6a54fa0
Saravana Kumar <saravanak@gmail.com>
2014-10-29 22:39:10 +0530

    issue.

--- app/models/product.rb
+++ app/models/product.rb
@@ -16,14 +16,14 @@ has_one :manufacturer,
 
 
  def self.search_type(type)
+
+   if (type.blank? || type.include?("Others"))
+      return =  [ "ItemtypeTag".camelize.constantize, "AttributeTag".camelize.constantize,"Topic".camelize.constantize,"Manufacturer".camelize.constantize, "CarGroup".camelize.constantize,"Mobile".camelize.constantize, "Tablet".camelize.constantize, "Car".camelize.constantize, "Camera".camelize.constantize,"Game".camelize.constantize,"Console".camelize.constantize,"WearableGadget".camelize.constantize,"Laptop".camelize.constantize,"Bike".camelize.constantize,"Cycle".camelize.constantize,"Tablet".camelize.constantize,"Hotel".camelize.constantize,"City".camelize.constantize]
+   end
    if type.is_a?(Array)
      return_val = type.collect{|t| t.to_s.gsub(/\s+/,'').strip.camelize.singularize.constantize}
    else
-     if (type.blank? || type.include?("Others"))
-      return_val =  [ "ItemtypeTag".camelize.constantize, "AttributeTag".camelize.constantize,"Topic".camelize.constantize,"Manufacturer".camelize.constantize, "CarGroup".camelize.constantize,"Mobile".camelize.constantize, "Tablet".camelize.constantize, "Car".camelize.constantize, "Camera".camelize.constantize,"Game".camelize.constantize,"Console".camelize.constantize,"WearableGadget".camelize.constantize,"Laptop".camelize.constantize,"Bike".camelize.constantize,"Cycle".camelize.constantize,"Tablet".camelize.constantize,"Hotel".camelize.constantize,"City".camelize.constantize]
-     else
       return_val = type.to_s.gsub(/\s+/,'').strip.camelize.singularize.constantize
-     end
    end
 
     
