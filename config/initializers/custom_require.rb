a73bb8a3f9287d1d2a7e67b7f11cb8b204a46c20
Sivakumar <siva@plannto.com>
2014-11-08 18:46:46 +0530

    fixes

--- config/initializers/custom_require.rb
+++ config/initializers/custom_require.rb
@@ -1,6 +1,12 @@
 %w{custom_acts_as_follower authentication reputation ext/string addressable/uri resque-retry resque/failure/redis amazon/ecs}.each { |each_val| require each_val }
 
-require "#{Rails.root}/lib/ecs"
+Amazon::Ecs.configure do |options|
+  options[:associate_tag] = 'pla04-21'
+  options[:AWS_access_key_id] = 'AKIAJWDCN4DJWNL2FK5A'
+  options[:AWS_secret_key] = '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'
+end
+
+Amazon::Ecs::SERVICE_URLS.merge!(:in => "http://webservices.amazon.in/onca/xml") # Added indian region
 
 if Rails.env.production?
   ENV['HEROKU_USER'] = "siva@plannto.com"
