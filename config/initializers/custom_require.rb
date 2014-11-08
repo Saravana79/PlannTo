ad0195206af48f668668f08d9ceb93df7371a337
Sivakumar <siva@plannto.com>
2014-11-08 18:40:00 +0530

    fixes

--- config/initializers/custom_require.rb
+++ config/initializers/custom_require.rb
@@ -1,12 +1,6 @@
 %w{custom_acts_as_follower authentication reputation ext/string addressable/uri resque-retry resque/failure/redis amazon/ecs}.each { |each_val| require each_val }
 
-# Amazon::Ecs.configure do |options|
-#   options[:associate_tag] = 'pla04-21'
-#   options[:AWS_access_key_id] = 'AKIAJWDCN4DJWNL2FK5A'
-#   options[:AWS_secret_key] = '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'
-# end
-
-# require "#{Rails.root}/lib/amazon/ecs"
+require "#{Rails.root}/lib/ecs"
 
 if Rails.env.production?
   ENV['HEROKU_USER'] = "siva@plannto.com"
