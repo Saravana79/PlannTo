97a53a8ab0339bd823939bb1d10569b2a8163841
Sivakumar <siva@plannto.com>
2014-11-08 18:44:31 +0530

    fixes

--- config/initializers/amazon.rb
+++ config/initializers/amazon.rb
@@ -3,3 +3,5 @@ Amazon::Ecs.configure do |options|
   options[:AWS_access_key_id] = 'AKIAJWDCN4DJWNL2FK5A'
   options[:AWS_secret_key] = '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'
 end
+
+Amazon::Ecs::SERVICE_URLS.merge!(:in => "http://webservices.amazon.in/onca/xml") # Added indian region
