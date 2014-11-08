c740b8e280602240d61686a2186c45cb83331ce4
Sivakumar <siva@plannto.com>
2014-11-08 18:16:51 +0530

    fixes

--- lib/amazon/ecs.rb
+++ lib/amazon/ecs.rb
@@ -1,17 +1,17 @@
-require "amazon/ecs"
-
-module Amazon
-  class Ecs
-
-    # SERVICE_URLS.merge!(:in => "http://webservices.amazon.in/onca/xml") # Added indian region
-
-    # def self.other_operation(item_id, opts={})
-    #   opts[:operation] = '[other valid operation supported by Product Advertising API]'
-    #
-    #   # setting default option value
-    #   opts[:item_id] = item_id
-    #
-    #   self.send_request(opts)
-    # end
-  end
-end
+# require "amazon/ecs"
+#
+# module Amazon
+#   class Ecs
+#
+#     # SERVICE_URLS.merge!(:in => "http://webservices.amazon.in/onca/xml") # Added indian region
+#
+#     # def self.other_operation(item_id, opts={})
+#     #   opts[:operation] = '[other valid operation supported by Product Advertising API]'
+#     #
+#     #   # setting default option value
+#     #   opts[:item_id] = item_id
+#     #
+#     #   self.send_request(opts)
+#     # end
+#   end
+# end
