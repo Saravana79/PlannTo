6a7d24d6d815c3bee5cff952490a2a2ba819cda0
Sivakumar <siva@plannto.com>
2014-11-10 19:08:17 +0530

    fixes

--- app/models/advertisement.rb
+++ app/models/advertisement.rb
@@ -406,12 +406,12 @@ class Advertisement < ActiveRecord::Base
     impression_date_condition = "impression_time > '#{from_date.beginning_of_day.strftime('%F %T')}' and impression_time < '#{end_date.end_of_day.strftime('%F %T')}'"
     click_date_condition = "timestamp > '#{from_date.beginning_of_day.strftime('%F %T')}' and timestamp < '#{end_date.end_of_day.strftime('%F %T')}'"
     if param[:select_by] == "item_id"
-      query = "select a.item_id,i.name,impressions_count,clicks_count from (select  ai.item_id, count(*) as impressions_count from add_impressions ai
+      query = "select a.item_id,i.name,impressions_count,clicks_count (clicks_count/impressions_count) as ectr from (select  ai.item_id, count(*) as impressions_count from add_impressions ai
                where advertisement_id = #{self.id} and #{impression_date_condition} group by item_id ) a left outer join (select item_id, count(*) as clicks_count from
                clicks where advertisement_id = #{self.id} and #{click_date_condition} group by item_id ) b on a.item_id= b.item_id inner join items i on i.id = a.item_id
                order by impressions_count desc"
     else
-      query = "select a.hosted_site_url,impressions_count,clicks_count from (select  ai.hosted_site_url, count(*) as impressions_count from add_impressions ai
+      query = "select a.hosted_site_url,impressions_count,clicks_count (clicks_count/impressions_count) as ectr from (select  ai.hosted_site_url, count(*) as impressions_count from add_impressions ai
                where advertisement_id = 1 and #{impression_date_condition} group by hosted_site_url ) a left outer join (select hosted_site_url, count(*) as
                clicks_count from clicks where advertisement_id = 1 and #{click_date_condition} group by hosted_site_url ) b on a.hosted_site_url= b.hosted_site_url
                order by impressions_count desc"
@@ -422,9 +422,9 @@ class Advertisement < ActiveRecord::Base
 
   def self.to_csv(param, reports)
     CSV.generate do |csv|
-      csv << [param[:select_by] == "item_id" ? "Item Name" : "Hosted Site Url", "Impressions Count", "Clicks Count"]
+      csv << [param[:select_by] == "item_id" ? "Item Name" : "Hosted Site Url", "Impressions Count", "Clicks Count", "ectr"]
       reports.each do |report|
-        csv << [*report.send(params[:select_by] == "item_id" ? "name" : "hosted_site_url"), report.impressions_count, report.clicks_count]
+        csv << [*report.send(params[:select_by] == "item_id" ? "name" : "hosted_site_url"), report.impressions_count, report.clicks_count, report.ectr.to_f.round(3)]
       end
     end
   end
