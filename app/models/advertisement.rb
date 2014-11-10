bf80654c1e6a731979be7a2591a5f9da431af310
Sivakumar <siva@plannto.com>
2014-11-10 17:49:34 +0530

    fixes

--- app/models/advertisement.rb
+++ app/models/advertisement.rb
@@ -406,7 +406,7 @@ class Advertisement < ActiveRecord::Base
     time_column = param[:select] == "add_impressions" ? "impression_time" : "timestamp"
     impression_date_condition = "#{time_column} > '#{from_date.beginning_of_day.strftime('%F %T')}' and #{time_column} < '#{end_date.end_of_day.strftime('%F %T')}'"
     if param[:select_by] == "item_id"
-      select_condition = "select i.name, count(*) as count from #{param[:select]} join items i on i.id = add_impressions.item_id"
+      select_condition = "select i.name as item_id, count(*) as count from #{param[:select]} join items i on i.id = add_impressions.item_id"
     else
       select_condition = "select hosted_site_url, count(*) as count from #{param[:select]}"
     end
