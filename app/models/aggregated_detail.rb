cc7a1cb83a7f8031fa843a2b4c44e194928413f1
Sivakumar <siva@plannto.com>
2014-11-14 18:31:41 +0530

    fixes

--- app/models/aggregated_detail.rb
+++ app/models/aggregated_detail.rb
@@ -24,7 +24,7 @@ class AggregatedDetail < ActiveRecord::Base
         winning_price = each_imp.winning_price
         if entity_type == "advertisement"
           commission = each_imp.commission.blank? ? 1 : each_imp.commission.to_f
-          winning_price = winning_price.to_f + (winning_price * (commission/100))
+          winning_price = winning_price.to_f + (winning_price.to_f * (commission/100))
         end
         aggregated_detail = AggregatedDetail.find_or_initialize_by_entity_id_and_date_and_entity_type(:entity_id => each_imp.entity_id, :date => time.to_date, :entity_type => entity_type)
         aggregated_detail.update_attributes(:impressions_count => each_imp.impression_count, :winning_price => winning_price)
