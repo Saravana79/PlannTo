56f7d7b5a238e21c974b52fc9a68c44637b552e3
Sivakumar <siva@plannto.com>
2014-11-20 16:52:38 +0530

    fixes

--- app/models/cookie_match.rb
+++ app/models/cookie_match.rb
@@ -1,5 +1,5 @@
 class CookieMatch < ActiveRecord::Base
-
+  validates_uniqueness_of :plannto_user_id
 
   scope :find_user, lambda {|plannto_user_id| where("plannto_user_id = '#{plannto_user_id}' and updated_at > '#{14.days.ago}'")}
 
@@ -35,33 +35,45 @@ class CookieMatch < ActiveRecord::Base
 
   def self.bulk_process_cookie_matching()
     length = $redis.llen("resque:queue:cookie_matching_process")
-    count = length
 
     source_categories = JSON.parse($redis.get("source_categories_pattern"))
     source_categories.default = {"pattern" => ""}
 
     begin
-      cookie_details = $redis.lrange("resque:queue:cookie_matching_process", 0, 1000)
+      cookie_details = cookie_details
+      cookies_arr = []
+      user_access_details = []
 
       cookie_details.each do |cookie_detail|
-        count-=1
-        cookie_detail = JSON.parse(cookie_detail)["args"][1]
-        cookie_detail["source"] ||= "google"
-        cookie_match = CookieMatch.find_or_initialize_by_plannto_user_id(cookie_detail["plannto_user_id"])
-        cookie_match.update_attributes(:google_user_id => cookie_detail["google_id"], :match_source => cookie_detail["source"])
+        begin
+          cookie_detail = JSON.parse(cookie_detail)["args"][1]
+          cookie_detail["source"] ||= "google"
+          ref_url = cookie_detail.delete("ref_url")
+          cookies_arr << cookie_detail
+          if !ref_url.blank?
+            param = {"plannto_user_id" => cookie_detail["plannto_user_id"], "ref_url" => ref_url, "source" => cookie_detail["source"]}
+            user_access_details << param
+          end
+        rescue Exception => e
+          p "There was problem while running cookie_match => #{e.backtrace}"
+        end
+      end
 
+      ActiveRecord::Base.transaction do
+        cookies_arr.each do |cookie_detail|
+          cookie_match = CookieMatch.find_or_initialize_by_plannto_user_id(cookie_detail["plannto_user_id"])
+          cookie_match.update_attributes(:google_user_id => cookie_detail["google_id"], :match_source => cookie_detail["source"])
+        end
+      end
+
+      cookies_arr.each do |cookie_detail|
         $redis_rtb.pipelined do
           $redis_rtb.set("cm:#{cookie_detail['google_id']}", cookie_detail["plannto_user_id"])
           $redis_rtb.expire("cm:#{cookie_detail['google_id']}", 2.weeks)
         end
-
-        if !cookie_detail["ref_url"].blank?
-          user_access_detail = UserAccessDetail.create(:plannto_user_id => cookie_detail["plannto_user_id"], :ref_url => cookie_detail["ref_url"], :source => cookie_detail["source"])
-
-          user_access_detail.update_buying_list_in_redis(source_categories)
-        end
-        p "Remaining CookieMatch Length - #{count}"
       end
+
+      UserAccessDetail.create(user_access_details)
 
       $redis.ltrim("resque:queue:cookie_matching_process", cookie_details.count, -1)
       length = length - cookie_details.count
