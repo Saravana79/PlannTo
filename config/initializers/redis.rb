16857d24fa5a78278c63a242f8ef0456f681e470
Sivakumar <siva@plannto.com>
2014-11-27 11:16:11 +0530

    ec2 changes

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,10 +1,10 @@
 #$redis = Redis.new(:host => 'localhost', :port => 6379)
-$redis = Redis.new(:host => "pub-redis-15107.us-east-1-2.2.ec2.garantiadata.com", :port => 15107, :password => "ehFuuSSR0ke8WWlD")
+# $redis = Redis.new(:host => "pub-redis-15107.us-east-1-2.2.ec2.garantiadata.com", :port => 15107, :password => "ehFuuSSR0ke8WWlD")
 
-# $redis = Redis.new(:host => "planntorep-001.l75bzd.0001.use1.cache.amazonaws.com")
+$redis = Redis.new(:host => "planntorep-001.l75bzd.0001.use1.cache.amazonaws.com")
 $redis_rtb = Redis.new(:host => 'rtbtarget.plannto.com', :port => 6379)
 
-# $redis_rtb = $redis_new = $redis
+#$redis_rtb = $redis
 
 Resque.redis = $redis
 #Resque::Server.use(Rack::Auth::Basic) do |user, password|
