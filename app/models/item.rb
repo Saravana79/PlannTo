df7eea4eaec561ba7aeea30de7ba0f3acee2cf41
Sivakumar <siva@plannto.com>
2014-11-28 12:15:24 +0530

    fixes for buyinglist process

--- app/models/item.rb
+++ app/models/item.rb
@@ -1455,77 +1455,79 @@ end
         redis_rtb_hash = {}
 
         user_vals.each do |each_user_val|
-          t_length-=1
-          # p "Remaining Length each - #{t_length} - #{Time.now.strftime("%H:%M:%S")}"
-          unless each_user_val.blank?
-            user_id, url, type, item_ids, advertisement_id = each_user_val.split("<<")
-            already_exist = Item.check_if_already_exist_in_user_visits(source_categories, user_id, url)
-            ranking = 0
+          begin
 
-            if already_exist == false
-              case type
-                when "Reviews", "Spec", "Photo"
-                  ranking = 10
-                when "Comparisons"
-                  ranking = 5
-                when "Lists", "Others"
-                  ranking = 2
-              end
+            t_length-=1
+            # p "Remaining Length each - #{t_length} - #{Time.now.strftime("%H:%M:%S")}"
+            unless each_user_val.blank?
+              user_id, url, type, item_ids, advertisement_id = each_user_val.split("<<")
+              already_exist = Item.check_if_already_exist_in_user_visits(source_categories, user_id, url, "users:last_visits")
+              ranking = 0
 
-              item_ids = item_ids.to_s.split(",")
-              if item_ids.count < 10
-
-                u_key = "u:ac:#{user_id}"
-                u_values = $redis.hgetall(u_key)
-
-                add_fad = u_values.blank? ? true : false
-
-                # Remove expired items from user details
-                u_values.each do |key,val|
-                  if key.include?("_la") && val.to_date < 2.weeks.ago.to_date
-                    item_id = key.gsub("_la", "")
-                    u_values.delete("#{item_id}_c")
-                    u_values.delete("#{item_id}_la")
-                  end
+              if already_exist == false
+                case type
+                  when "Reviews", "Spec", "Photo"
+                    ranking = 10
+                  when "Comparisons"
+                    ranking = 5
+                  when "Lists", "Others"
+                    ranking = 2
                 end
 
-                # incrby count for items
-                item_ids.each do |each_key|
-                  if u_values["#{each_key}_c"].blank?
-                    u_values["#{each_key}_c"] = ranking
-                    u_values["#{each_key}_la"] = Date.today.to_s
-                  else
-                    old_ranking = u_values["#{each_key}_c"]
-                    u_values["#{each_key}_c"] = old_ranking.to_i + ranking
-                    u_values["#{each_key}_la"] = Date.today.to_s
-                  end
-                end
+                item_ids = item_ids.to_s.split(",")
+                if item_ids.count < 10
 
-                proc_item_ids = u_values.map {|key,val| if key.include?("_c") && val.to_i > 30; key.gsub("_c",""); end}.compact
+                  u_key = "u:ac:#{user_id}"
+                  u_values = $redis.hgetall(u_key)
 
-                old_buying_list = u_values["buyinglist"]
-                buying_list = old_buying_list.to_s.split(",")
+                  add_fad = u_values.blank? ? true : false
 
-                if !buying_list.blank?
-                  have_to_del = []
-
-                  #remove items from buyinglist which detail is expired
-                  buying_list.each do |each_item|
-                    if !u_values.include?("#{each_item}_c") || u_values["#{each_item}_c"].to_i < 30
-                      have_to_del << each_item
+                  # Remove expired items from user details
+                  u_values.each do |key,val|
+                    if key.include?("_la") && val.to_date < 2.weeks.ago.to_date
+                      item_id = key.gsub("_la", "")
+                      u_values.delete("#{item_id}_c")
+                      u_values.delete("#{item_id}_la")
                     end
                   end
 
-                  buying_list = buying_list - have_to_del
-                  buying_list << proc_item_ids
-                  buying_list = buying_list.flatten.uniq
-                else
-                  buying_list = proc_item_ids
-                end
+                  # incrby count for items
+                  item_ids.each do |each_key|
+                    if u_values["#{each_key}_c"].blank?
+                      u_values["#{each_key}_c"] = ranking
+                      u_values["#{each_key}_la"] = Date.today.to_s
+                    else
+                      old_ranking = u_values["#{each_key}_c"]
+                      u_values["#{each_key}_c"] = old_ranking.to_i + ranking
+                      u_values["#{each_key}_la"] = Date.today.to_s
+                    end
+                  end
 
-                buying_list = buying_list.delete_if {|each_item| base_item_ids.include?(each_item)}
+                  proc_item_ids = u_values.map {|key,val| if key.include?("_c") && val.to_i > 30; key.gsub("_c",""); end}.compact
 
-                # if !old_buying_list.blank? || !buying_list.blank?
+                  old_buying_list = u_values["buyinglist"]
+                  buying_list = old_buying_list.to_s.split(",")
+
+                  if !buying_list.blank?
+                    have_to_del = []
+
+                    #remove items from buyinglist which detail is expired
+                    buying_list.each do |each_item|
+                      if !u_values.include?("#{each_item}_c") || u_values["#{each_item}_c"].to_i < 30
+                        have_to_del << each_item
+                      end
+                    end
+
+                    buying_list = buying_list - have_to_del
+                    buying_list << proc_item_ids
+                    buying_list = buying_list.flatten.uniq
+                  else
+                    buying_list = proc_item_ids
+                  end
+
+                  buying_list = buying_list.delete_if {|each_item| base_item_ids.include?(each_item)}
+
+                  # if !old_buying_list.blank? || !buying_list.blank?
                   items_hash = u_values.select {|k,_| k.include?("_c")}
                   items_count = items_hash.count
                   all_item_ids = Hash[items_hash.sort_by {|_,v| v.to_i}.reverse].map {|k,_| k.gsub("_c","")}.compact
@@ -1533,29 +1535,32 @@ end
                   temp_store = {"item_ids" => u_values["buyinglist"], "count" => items_count, "all_item_ids" => all_item_ids, "lad" => Date.today.to_s}
                   temp_store = temp_store.merge("fad" => Date.today.to_s) if add_fad
                   redis_rtb_hash.merge!("users:buyinglist:#{user_id}" => temp_store)
-                # end
+                  # end
 
-                begin
-                  if u_values.blank?
-                    del_key = "users:buyinglist:#{user_id}"
-                    $redis.del(u_key)
-                    $redis_rtb.del(del_key)
-                    redis_rtb_hash.delete(del_key)
-                  else
-                    $redis.pipelined do
-                      $redis.hmset(u_key, u_values.flatten)
-                      $redis.expire(u_key, 2.weeks)
+                  begin
+                    if u_values.blank?
+                      del_key = "users:buyinglist:#{user_id}"
+                      $redis.del(u_key)
+                      $redis_rtb.del(del_key)
+                      redis_rtb_hash.delete(del_key)
+                    else
+                      $redis.pipelined do
+                        $redis.hmset(u_key, u_values.flatten)
+                        $redis.expire(u_key, 2.weeks)
+                      end
                     end
+                  rescue Exception => e
+                    p "Problems while hmset: Args:-"
+                    p u_key
+                    p u_values
+                    p u_values.flatten
+                    raise e
                   end
-                rescue Exception => e
-                  p "Problems while hmset: Args:-"
-                  p u_key
-                  p u_values
-                  p u_values.flatten
-                  raise e
                 end
               end
             end
+          rescue Exception => e
+            p "Error While Processing => #{e}"
           end
         end
 
