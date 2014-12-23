31e122e13a85b50e2a85f34d972c36f30a389225
Saravana Kumar <saravanak@gmail.com>
2014-12-23 15:00:24 +0530

    feed and style is changed.

--- app/models/feed.rb
+++ app/models/feed.rb
@@ -86,7 +86,7 @@ class Feed < ActiveRecord::Base
   end
 
   def table_process()
-    @impression_missing = ImpressionMissing.where("updated_at > ? and count > ?", (self.last_updated_at.blank? ? Time.zone.now-2.days : self.last_updated_at), 50)
+    @impression_missing = ImpressionMissing.where("updated_at > ? and count > ?", (self.last_updated_at.blank? ? Time.zone.now-2.days : self.last_updated_at), 5)
     admin_user = User.where(:is_admin => true).first
 
     @impression_missing.each do |each_record|
