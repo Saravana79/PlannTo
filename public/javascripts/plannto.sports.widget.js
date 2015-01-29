f81342ec2a065c432e9a050593137b7f7433ce30
Sivakumar <siva@plannto.com>
2015-01-29 22:04:10 +0530

    fixes for sports

--- public/javascripts/plannto.sports.widget.js
+++ public/javascripts/plannto.sports.widget.js
@@ -10,9 +10,9 @@ var PlannTo = (function(window,undefined) {
     var PlannTo ={};
     var SubPath="/sports_widget.js"
 //for production
-//    var domain = "www.plannto.com";
+    var domain = "www.plannto.com";
 //for development
-var domain = "localhost:3000";
+//var domain = "localhost:3000";
 // Localize jQuery variable
     var jQuery;
 
