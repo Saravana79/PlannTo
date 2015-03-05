d2d87211e955ba13b738932cd104dfff1224f30b
Saravana Kumar <saravanak@gmail.com>
2015-03-05 06:08:37 +0530

    modified the hard coded sportskeeda url.

--- app/models/publisher.rb
+++ app/models/publisher.rb
@@ -4,6 +4,7 @@ class Publisher < ActiveRecord::Base
 		host = URI.parse(url).host.downcase rescue ""
 		domain = host.start_with?('www.') ? host[4..-1] : host
 		domain = domain.start_with?('blog.') ? domain[5..-1] : domain
-		publisher = Publisher.where(:publisher_url => "sportskeeda.com").first
+		#publisher = Publisher.where(:publisher_url => "sportskeeda.com").first
+		publisher = Publisher.where(domain).first
 	end
 end
