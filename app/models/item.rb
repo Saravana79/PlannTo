f1c01cba5ba736912c4e38b30ae61f9ba880f585
Sivakumar <siva@plannto.com>
2015-04-08 18:24:35 +0530

    fixes for item

--- app/models/item.rb
+++ app/models/item.rb
@@ -2372,14 +2372,14 @@ end
       item_name = "WomenTop"
     elsif sample_int < 52
       item_name = "DressMaterial"
-    elsif sample_int < 62
+    elsif sample_int < 70 #TODO: Added +8 temp
       item_name = "Kurta"
-    elsif sample_int < 66
+    elsif sample_int < 74
       item_name = "Sunglass"
-    elsif sample_int < 68
-      item_name = "Underwear"
-    elsif sample_int < 72
-      item_name = "Legging"
+    # elsif sample_int < 74
+    #   item_name = "Underwear"
+    # elsif sample_int < 74
+    #   item_name = "Legging"
     elsif sample_int < 82
       item_name = "Dress"
     elsif sample_int < 90
