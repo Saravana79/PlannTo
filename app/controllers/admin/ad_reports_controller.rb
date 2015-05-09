b1915e954564dd657646e09d4a446d26a2e0ca36
Sivakumar <siva@plannto.com>
2015-05-09 19:12:27 +0530

    only show top 50 items reports

--- app/controllers/admin/ad_reports_controller.rb
+++ app/controllers/admin/ad_reports_controller.rb
@@ -139,7 +139,7 @@ class Admin::AdReportsController < ApplicationController
     @ad_types = ["advertisement", "non advertisement"]
     @advertisements = ["All"] + Advertisement.all.map(&:id)
 
-    @results = AggregatedImpression.get_results_from_agg_impression(params, @start_date, @end_date).first(100)
+    @results = AggregatedImpression.get_results_from_agg_impression(params, @start_date, @end_date).first(50)
     item_ids = @results.map {|k, _| k}.compact.map(&:to_i)
     @items = Item.where(:id => item_ids)
   end
