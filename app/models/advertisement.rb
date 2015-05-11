e1296af2fce08241d5308433947c33f32762d932
Sivakumar <siva@plannto.com>
2015-05-11 15:26:03 +0530

    fixes for fetching itemtype_id

--- app/models/advertisement.rb
+++ app/models/advertisement.rb
@@ -1169,7 +1169,7 @@ class Advertisement < ActiveRecord::Base
         redis_rtb_hash = {}
         non_ad_impressions_list.each do |impression|
           begin
-            article_content = ArticleContent.find_by_sql("select sub_type,group_concat(icc.item_id) all_item_ids, ac.id from article_contents ac inner join contents c on ac.id = c.id
+            article_content = ArticleContent.find_by_sql("select itemtype_id,sub_type,group_concat(icc.item_id) all_item_ids, ac.id from article_contents ac inner join contents c on ac.id = c.id
 inner join item_contents_relations_cache icc on icc.content_id = ac.id
 where url = '#{impression.hosted_site_url}' group by ac.id").first
 
