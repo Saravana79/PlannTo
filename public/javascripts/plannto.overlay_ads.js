90fdf0feb8f6257f6159c26011b158477d86d6e4
Sivakumar <siva@plannto.com>
2015-08-25 13:11:39 +0530

    product url changed

--- public/javascripts/plannto.overlay_ads.js
+++ public/javascripts/plannto.overlay_ads.js
@@ -10,9 +10,9 @@ var PlannTo = (function(window,undefined) {
     var PlannTo ={};
 //    var SubPath="/price_vendor_details.js"
 //for production
-//    var domain = "www.plannto.com";
+    var domain = "www.plannto.com";
 //for development
-var domain = "localhost:3000";
+//var domain = "localhost:3000";
 // Localize jQuery variable
     var jQuery;
 
