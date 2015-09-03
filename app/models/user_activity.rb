f58ad4236516b22d5f0ffce38e76fd2d8b11a1ab
Sivakumar <siva@plannto.com>
2015-09-03 16:26:11 +0530

    user activity

--- app/models/user_activity.rb
+++ app/models/user_activity.rb
@@ -2,15 +2,17 @@ class UserActivity < ActiveRecord::Base
  belongs_to :user
  
  def self.save_user_activity(current_user,related_id,related_activity,related_activity_type,activity_id,remote_ip)
-    u = UserActivity.new
-    u.user_id = current_user.id
-    u.related_id = related_id
-    u.related_activity = related_activity
-    u.related_activity_type = related_activity_type
-    u.time = Time.zone.now
-    u.activity_id = activity_id
-    u.ip_address = remote_ip
-    u.save   
+   if !current_user.blank?
+     u = UserActivity.new
+     u.user_id = current_user.id
+     u.related_id = related_id
+     u.related_activity = related_activity
+     u.related_activity_type = related_activity_type
+     u.time = Time.zone.now
+     u.activity_id = activity_id
+     u.ip_address = remote_ip
+     u.save
+   end
  end
  
   def sort_by
