16ea5e82115c3dcb5ace28090352f077b45e7a39
Saravana Kumar <saravanak@gmail.com>
2015-09-17 09:46:03 +0530

    rolled back changes.

--- app/models/vendor_detail.rb
+++ app/models/vendor_detail.rb
@@ -23,7 +23,7 @@ class VendorDetail < ActiveRecord::Base
     vendor_details = {}
     details = find_by_sql("SELECT item_id, name as vendor_name, imageurl, default_text FROM `vendor_details` WHERE item_id in (#{item_ids.blank? ? "''" : item_ids.map(&:inspect).join(', ')})")
 
-    details.map {|each_detail| vendor_details.merge!(each_detail.item_id => each_detail.attributes.merge('imageurl' => configatron.root_image_url + "vendor" + '/small/' + each_detail.imageurl.to_s))}
+    details.map {|each_detail| vendor_details.merge!(each_detail.item_id => each_detail.attributes.merge('imageurl' => configatron.root_image_url + "vendor" + '/medium/' + each_detail.imageurl.to_s))}
 
     # Vendor.where("id in (?)", item_ids).map {|each_vendor| vendor_details.merge!(each_vendor.id => {:image_url => each_vendor.image_url, :default_text => each_vendor.default_text, :vendor_name => each_vendor.name})}
     vendor_details
