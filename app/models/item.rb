b1b85f30101e0495483927cf6b47e3ac99d1bf17
Saravana Kumar <saravanak@gmail.com>
2015-10-12 17:53:45 +0530

    added the order by.

--- app/models/item.rb
+++ app/models/item.rb
@@ -3054,9 +3054,9 @@ end
     if !@items.blank?
       item = @items.first
       if !publisher.blank? && !publisher.vendor_ids.blank?
-        @itemdetail = item.itemdetails.where(:site => publisher.vendor_ids.to_s.split(","), :IsError => false).first
+        @itemdetail = item.itemdetails.where(:site => publisher.vendor_ids.to_s.split(","), :IsError => false, :status => 1).order(:price).first
       else
-        @itemdetail = item.itemdetails.where(:IsError => false).first
+        @itemdetail = item.itemdetails.where(:IsError => false, :status => 1).order(:price).first
       end
     end
     @itemdetail
