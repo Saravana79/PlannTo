efe8219c3e82220ae1314debb59ffeddc74b7768
Sivakumar <siva@plannto.com>
2015-11-25 13:16:10 +0530

    plannto overlay image ads fixes

--- public/javascripts/plannto.overlay_ads.js
+++ public/javascripts/plannto.overlay_ads.js
@@ -97,94 +97,6 @@ var PlannTo = (function(window,undefined) {
         return null;
     }
 
-    pln_off = function(el) {
-        var width = el.offsetWidth,
-            height = el.offsetHeight,
-            top = 0, left = 0,
-            rect = false, w = window, d = document, de = d.documentElement, b = d.body;
-        if (el.getBoundingClientRect) {
-            try {
-                rect = el.getBoundingClientRect();
-                top = ~~(rect.top) + Math.max(w.pageYOffset||0,de.scrollTop,b.scrollTop,0) - Math.max(de.clientTop,b.clientTop,0);
-                left = ~~(rect.left) + Math.max(w.pageXOffset||0,de.scrollLeft,b.scrollLeft,0) - Math.max(de.clientLeft,b.clientLeft,0);
-            } catch(e){ rect = false; top = 0; left = 0; }
-        }
-        if (!rect) {
-            do {
-                top += el.offsetTop;
-                left += el.offsetLeft;
-            } while ((el = el.offsetParent));
-        }
-        return {
-            top: top,
-            t : top,
-            bottom: top+height,
-            b : top+height,
-            left: left,
-            l : left,
-            right: left + width,
-            r : left + width,
-            height: height,
-            h : height,
-            width: width,
-            w : width
-        };
-    };
-
-    reposition = function() {
-        jQuery.each(valid_images, function(indx, image) {
-            if (indx != 0)
-                return
-
-            indx = indx + 1;
-            var off_d = pln_off(image)
-
-            console.log(off_d)
-            console.log(n_ratio)
-            console.log(e_ratio)
-
-            if (n_ratio != 0.0)
-            {
-                n_ad_height = Math.round(off_d.width / n_ratio)
-                n_ad_height_w_close_btn = n_ad_height + 20
-                console.log(n_ad_height)
-            }
-
-            var img_h = off_d.height - n_ad_height_w_close_btn
-            var off_top = off_d.top + img_h
-
-            if (e_ratio != 0.0)
-            {
-                e_ad_height = Math.round(off_d.width / e_ratio)
-                e_ad_height_w_close_btn = e_ad_height + 20
-                console.log(n_ad_height)
-            }
-
-            var exp_img_h = off_d.height - e_ad_height_w_close_btn
-            var exp_off_top = off_d.top + exp_img_h
-
-            console.log(exp_off_top)
-            console.log(pln_frame_expanded)
-            console.log(jQuery(".img_overlay_img").width())
-
-//            var ratio = 970/img_width
-//            ad_height = 400/ratio
-
-            if (pln_frame_expanded == true)
-            {
-                jQuery(".plannto_in_image_ad_1").css({"width":off_d.width+"px", "height": e_ad_height_w_close_btn+"px", "top":exp_off_top+"px", "left":off_d.left+"px"})
-            }
-            else if (pln_frame_closed == false)
-            {
-                jQuery(".plannto_in_image_ad_1").css({"top":off_top+"px","left":off_d.left+"px","width":off_d.width+"px", "height": n_ad_height_w_close_btn+"px"})
-            }
-            else
-            {
-                jQuery(".plannto_in_image_ad_1").css({"top":off_top+"px","left":off_d.left+"px","width":off_d.width+"px", "height": n_ad_height_w_close_btn+"px"})
-            }
-        });
-    }
-
     /******** Main function ********/
     function main() {
 
@@ -205,9 +117,99 @@ var PlannTo = (function(window,undefined) {
             e_ad_height = 300
             e_ad_height_w_close_btn = 500
 
+
+            pln_off = function(el) {
+                var width = el.offsetWidth,
+                    height = el.offsetHeight,
+                    top = 0, left = 0,
+                    rect = false, w = window, d = document, de = d.documentElement, b = d.body;
+                if (el.getBoundingClientRect) {
+                    try {
+                        rect = el.getBoundingClientRect();
+                        top = ~~(rect.top) + Math.max(w.pageYOffset||0,de.scrollTop,b.scrollTop,0) - Math.max(de.clientTop,b.clientTop,0);
+                        left = ~~(rect.left) + Math.max(w.pageXOffset||0,de.scrollLeft,b.scrollLeft,0) - Math.max(de.clientLeft,b.clientLeft,0);
+                    } catch(e){ rect = false; top = 0; left = 0; }
+                }
+                if (!rect) {
+                    do {
+                        top += el.offsetTop;
+                        left += el.offsetLeft;
+                    } while ((el = el.offsetParent));
+                }
+                return {
+                    top: top,
+                    t : top,
+                    bottom: top+height,
+                    b : top+height,
+                    left: left,
+                    l : left,
+                    right: left + width,
+                    r : left + width,
+                    height: height,
+                    h : height,
+                    width: width,
+                    w : width
+                };
+            };
+
+            reposition = function() {
+                console.log(5858585858)
+                jQuery.each(valid_images, function(indx, image) {
+                    if (indx != 0)
+                        return
+
+                    indx = indx + 1;
+                    var off_d = pln_off(image)
+
+                    console.log(off_d)
+                    console.log(n_ratio)
+                    console.log(e_ratio)
+
+                    if (n_ratio != 0.0)
+                    {
+                        n_ad_height = Math.round(off_d.width / n_ratio)
+                        n_ad_height_w_close_btn = n_ad_height + 20
+                        console.log(n_ad_height)
+                    }
+
+                    var img_h = off_d.height - n_ad_height_w_close_btn
+                    var off_top = off_d.top + img_h
+
+                    if (e_ratio != 0.0)
+                    {
+                        e_ad_height = Math.round(off_d.width / e_ratio)
+                        e_ad_height_w_close_btn = e_ad_height + 20
+                        console.log(n_ad_height)
+                    }
+
+                    var exp_img_h = off_d.height - e_ad_height_w_close_btn
+                    var exp_off_top = off_d.top + exp_img_h
+
+                    console.log(exp_off_top)
+                    console.log(pln_frame_expanded)
+                    console.log(jQuery(".img_overlay_img").width())
+
+//            var ratio = 970/img_width
+//            ad_height = 400/ratio
+
+                    if (pln_frame_expanded == true)
+                    {
+                        jQuery(".plannto_in_image_ad_1").css({"width":off_d.width+"px", "height": e_ad_height_w_close_btn+"px", "top":exp_off_top+"px", "left":off_d.left+"px"})
+                    }
+                    else if (pln_frame_closed == false)
+                    {
+                        jQuery(".plannto_in_image_ad_1").css({"top":off_top+"px","left":off_d.left+"px","width":off_d.width+"px", "height": n_ad_height_w_close_btn+"px"})
+                    }
+                    else
+                    {
+                        jQuery(".plannto_in_image_ad_1").css({"top":off_top+"px","left":off_d.left+"px","width":off_d.width+"px", "height": n_ad_height_w_close_btn+"px"})
+                    }
+                });
+            }
+
             jQuery(window).bind('resize', reposition);
 
-            sto = setInterval(reposition, 1000);
+//            sto = setInterval(reposition, 1000);
 
             valid_images = []
 
@@ -271,6 +273,10 @@ var PlannTo = (function(window,undefined) {
                             n_ad_height = data.n_ad_height
                             e_ad_height = data.e_ad_height
                             expand_type = data.expand_type
+                            expand_on = data.expand_on
+                            need_close_btn = data.need_close_btn
+                            console.log(expand_on)
+                            console.log(need_close_btn)
 
                             n_ad_height_w_close_btn = n_ad_height + 20
                             e_ad_height_w_close_btn = e_ad_height + 20
@@ -309,6 +315,12 @@ var PlannTo = (function(window,undefined) {
                             jQuery("body").append('<div class="plan_main_div_image_1" style="padding: 0px; margin: 0px; border: medium none; background: transparent none repeat scroll 0px 0px; position: static;"> ' +
                                 '<div class="plannto_in_image_ad_1" style="margin: 0px; top: '+ off_top +'px; right: 0px; height: '+ n_ad_height_w_close_btn +'px; left: '+ off_d.left +'px; overflow: hidden; padding: 0px; position: absolute; visibility: visible; width: '+ off_d.width +'px; z-index: 10000;"><div class="plannto_iframe" style="position: relative; height: 100%; width: 100%; background: transparent none repeat scroll 0% 0%; color: inherit; font: 12px/0.5 Arial; margin-top: 0px; opacity: 1; bottom: 0px;"><img src="http://cdn1.plannto.com/static/images/ad_close.png" class="clsbtn close_plannto_iframe"><span style="width:20px;float:right;display:block;height:20px;"><a href="#" class="close_plannto_iframe"></a></span>' +
                                 '<span class="plannto_ad_tag" style="margin-right:5px;font-weight:600;color:#808080;font-family:sans-serif;font-size:11px;padding-top:7px;height:13px;right:24px;position:absolute;"><a style="text-decoration:none;font-weight: 600;color: #808080;font-family: sans-serif;font-size: 11px;" href="http://www.plannto.com" target="_blank">ADS BY PlannTo</a></span> <div id="plannto_ad_frame" style="border:medium none;position:absolute;z-index:'+ z_index +';bottom:0px;background-color:white;" height= '+ n_ad_height +'px width="'+img_width+'px"> </div><div id="exp_plannto_ad_frame" style="border:medium none;position:absolute;bottom:0px;display:none;" height='+e_ad_height+'px width="'+img_width+'px"> </div></div><div class="plannto_hint_button plannto_hint_button'+indx+'"></div></div></div>')
+
+
+//                            if ()
+//                            {
+//
+//                            }
 
 
 //                            jQuery("#plannto_ad_frame").attr("src","data:text/html;charset=utf-8," + encodeURI(data.html))
