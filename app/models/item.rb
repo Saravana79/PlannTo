80041553ec5df9c3ec012e5ceb470e9229c5284d
Sivakumar <siva@plannto.com>
2015-12-07 20:15:01 +0530

    fixes for item

--- app/models/item.rb
+++ app/models/item.rb
@@ -1637,7 +1637,7 @@ end
       length = $redis.llen("resque:queue:buying_list_process")
       if length < 50
         [*length...50].each do |each_count|
-          user_vals = $redis_rtb.lrange("users:visits", 0, 10000)
+          user_vals = $redis_rtb.lrange("users:visits", 0, 5000)
           Resque.enqueue(BuyingListProcess, "buying_list_process_in_redis", Time.zone.now.utc, user_vals)
           $redis_rtb.ltrim("users:visits", user_vals.count, -1)
         end
