da4806d1e7236c0d65ec81398da9af8f0f870a1f
Sivakumar <siva@plannto.com>
2015-12-16 10:52:44 +0530

    items controller fixes

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -7,7 +7,7 @@ class ItemsController < ApplicationController
 #    c.params
 #   end
 #     },:expires_in => 2.hour
-  layout "product"
+  layout "product", :except => [:compare]
   before_filter :authenticate_user!, :except => [:index, :compare]
   skip_before_filter :cache_follow_items, :store_session_url, :only => [:compare]
 
@@ -247,40 +247,40 @@ class ItemsController < ApplicationController
 
     def compare
       return render :nothing => true
-      @static_page1 = "true"
-      @ids = params[:ids].split(',').uniq rescue ""
-      p = params[:ids].split(',').uniq rescue ''
-      unless p == ""
-        if p.include?('')
-          p.delete('')
-          params[:ids] = p.join(",")
-        end
-      end
-
-
-      unless @ids.blank? || params[:ids] == ""
-        related_item_ids = RelatedItem.where('item_id in (?)',p).order(:variance).limit(10).collect(&:related_item_id)
-        @related_items = Item.where('id in (?) and id not in (?)' ,related_item_ids,  p).order("id desc")
-        @items = Item.includes([:attribute_values => :attribute],:item_rating).where("items.id in (?)", p)
-
-        @item1 = @items[0]
-        session[:item_type] =  @item1.get_base_itemtype rescue ""
-        logger.info "======================++++++====#{@items}"
-        #   @attribute_ids = @items.collect{|i|i.attribute_values.collect(&:attribute_id)}.flatten.uniq
-        @attributes = @items.collect(&:attribute_values).flatten.uniq.collect{|av| av.attribute}.flatten.uniq.compact
-        @attribute_ids = @attributes.collect(&:id)
-        @ids[0] = "0" if @ids[0] == ""
-        content_ids = ItemContentsRelationsCache.find_by_sql("select content_id, count(*) from item_contents_relations_cache INNER JOIN contents ON item_contents_relations_cache.content_id = contents.id where item_id in (#{p.join(",")}) and contents.sub_type in ('Comparisons') group by content_id having count(*) > 1 limit 15").collect(&:content_id)
-        @contents = Content.where("id in (?)",content_ids).order("total_votes desc")
-        @attribute_comparision_lists = AttributeComparisonList.where(:itemtype_id => @item1.itemtype_id).order("sort_order")
-
-      else
-
-        unless params[:item_type].nil?
-          session[:item_type] = params[:item_type]
-        end
-        @item_type = session[:item_type]
-      end
+      # @static_page1 = "true"
+      # @ids = params[:ids].split(',').uniq rescue ""
+      # p = params[:ids].split(',').uniq rescue ''
+      # unless p == ""
+      #   if p.include?('')
+      #     p.delete('')
+      #     params[:ids] = p.join(",")
+      #   end
+      # end
+      #
+      #
+      # unless @ids.blank? || params[:ids] == ""
+      #   related_item_ids = RelatedItem.where('item_id in (?)',p).order(:variance).limit(10).collect(&:related_item_id)
+      #   @related_items = Item.where('id in (?) and id not in (?)' ,related_item_ids,  p).order("id desc")
+      #   @items = Item.includes([:attribute_values => :attribute],:item_rating).where("items.id in (?)", p)
+      #
+      #   @item1 = @items[0]
+      #   session[:item_type] =  @item1.get_base_itemtype rescue ""
+      #   logger.info "======================++++++====#{@items}"
+      #   #   @attribute_ids = @items.collect{|i|i.attribute_values.collect(&:attribute_id)}.flatten.uniq
+      #   @attributes = @items.collect(&:attribute_values).flatten.uniq.collect{|av| av.attribute}.flatten.uniq.compact
+      #   @attribute_ids = @attributes.collect(&:id)
+      #   @ids[0] = "0" if @ids[0] == ""
+      #   content_ids = ItemContentsRelationsCache.find_by_sql("select content_id, count(*) from item_contents_relations_cache INNER JOIN contents ON item_contents_relations_cache.content_id = contents.id where item_id in (#{p.join(",")}) and contents.sub_type in ('Comparisons') group by content_id having count(*) > 1 limit 15").collect(&:content_id)
+      #   @contents = Content.where("id in (?)",content_ids).order("total_votes desc")
+      #   @attribute_comparision_lists = AttributeComparisonList.where(:itemtype_id => @item1.itemtype_id).order("sort_order")
+      #
+      # else
+      #
+      #   unless params[:item_type].nil?
+      #     session[:item_type] = params[:item_type]
+      #   end
+      #   @item_type = session[:item_type]
+      # end
     end
 
   def remove_duplicate_item
