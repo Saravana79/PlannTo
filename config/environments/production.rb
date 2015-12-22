0ff6d416f4c7c60b3a2ae2943c42702c07967c7e
Sivakumar <siva@plannto.com>
2015-12-22 11:41:21 +0530

    removed spooling attack

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,4 +77,6 @@ PlanNto::Application.configure do
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
+
+  config.action_dispatch.ip_spoofing_check = false
 end
