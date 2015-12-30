f301bdc38cc61df3daecc4242583109965724a3e
Sivakumar <siva@plannto.com>
2015-12-30 18:27:21 +0530

    fixes

--- config/initializers/interpolations.rb
+++ config/initializers/interpolations.rb
@@ -10,15 +10,22 @@
 # end
 
 Paperclip.interpolates(:image_prefix_path) do |attachment, style|
-  itemdetail = attachment.instance.imageable rescue nil
-  if_item_detail = itemdetail.blank? ? "" : itemdetail.class.to_s.downcase rescue ""
-  image_type = attachment.instance.imageable.type.downcase rescue "ad"
-  table_name = attachment.instance.imageable.class.table_name
-  if (if_item_detail == "itemdetail" || if_item_detail == "itembeautydetail")
-    vendor_name = itemdetail.get_vendor_name rescue nil
+  image_obj = attachment.instance
+  imageable_class = image_obj.imageable_type.to_s
+  if (imageable_class == "Itemdetail" || imageable_class == "ItemBeautyDetail")
+    vendor_name = image_obj.imageable.get_vendor_name rescue nil
     vendor_name = vendor_name.blank? ? "MySmartPrice" : vendor_name
     return_val = "vendors/#{vendor_name}"    #TODO: hot code value for MySmartPrice
+  elsif imageable_class == "ItemDetailOther"
+    return_val = "item_detail_others/ad"
+  elsif imageable_class == "Advertisement"
+    return_val = "advertisements/images/:id"
+  elsif imageable_class == "Item"
+    image_type = image_obj.imageable.type.downcase rescue "item"
+    return_val = "items/#{image_type}"
   else
+    table_name = image_obj.imageable.class.table_name
+    image_type = image_obj.imageable.type.downcase rescue "item"
     return_val = "#{table_name}/#{image_type}"
   end
   return_val
