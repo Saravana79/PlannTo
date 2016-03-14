660068243957f3676f58e5656db7346992350afe
Sivakumar <siva@plannto.com>
2016-03-14 13:25:39 +0530

    web concurrency set to 2

--- config/puma.rb
+++ config/puma.rb
@@ -1,13 +1,13 @@
 require "puma"
-workers Integer(ENV['WEB_CONCURRENCY'] || 1)
-threads_count = Integer(ENV['MAX_THREADS'] || 2)
+workers Integer(ENV["WEB_CONCURRENCY"] || 2)
+threads_count = Integer(ENV["MAX_THREADS"] || 2)
 threads threads_count, threads_count
 
 preload_app!
 
 rackup      DefaultRackup
-port        ENV['PORT']     || 3000
-environment ENV['RACK_ENV'] || 'development'
+port        ENV["PORT"]     || 3000
+environment ENV["RACK_ENV"] || 'development'
 
 on_worker_boot do
   # Worker specific setup for Rails 4.1+
