2082ecaf25cfa26f39585f0c94a6a6a5545bdb1b
Sivakumar <siva@plannto.com>
2016-03-18 18:26:37 +0530

    fixes

--- config/puma.rb
+++ config/puma.rb
@@ -1,6 +1,6 @@
 require "puma"
-workers Integer(ENV["WEB_CONCURRENCY"] || 2)
-threads_count = Integer(ENV["MAX_THREADS"] || 2)
+workers Integer(ENV["WEB_CONCURRENCY"] || 5)
+threads_count = Integer(ENV["MAX_THREADS"] || 1)
 threads threads_count, threads_count
 
 preload_app!
