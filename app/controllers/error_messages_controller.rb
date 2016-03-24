5e78a7867f0e53bd5340b6f917043eade3297dba
Sivakumar <siva@plannto.com>
2016-03-24 13:06:14 +0530

    fixes for error messages

--- app/controllers/error_messages_controller.rb
+++ app/controllers/error_messages_controller.rb
@@ -1,9 +1,11 @@
 class ErrorMessagesController < ApplicationController
   before_filter :authenticate_admin_user!
+  layout 'product'
   
   def index
-    @error_messages = ErrorMessage.order('created_at desc').paginate :page => params[:page],:per_page => 10
+    @error_messages = ErrorMessage.order('created_at desc').paginate :page => params[:page],:per_page => 20
   end
+
   def show
     @error_message = ErrorMessage.find(params[:id])
   end 
