9bd716f17c16290fbdeb791c9bcf3d42c43aba9c
Sivakumar <siva@plannto.com>
2016-05-02 12:11:37 +0530

    fixes for related items

--- app/controllers/related_items_controller.rb
+++ app/controllers/related_items_controller.rb
@@ -8,15 +8,19 @@ class RelatedItemsController < ApplicationController
     # @related_items = Item.where(:id => related_item_ids,:itemtype_id => item.itemtype.id).includes(:item_rating,:attribute_values).order("id desc").paginate(:page => params[:page], :per_page => 10)
     #@related_items = Item.find(related_item_ids, :order => "field(id, #{related_item_ids.map(&:inspect).join(',')})")
 
-    @related_items = Item.where(:id => related_item_ids)
-    @related_items = related_item_ids.collect {|id| @related_items.detect {|x| x.id == id}}
-    @related_items = @related_items.uniq.compact
-
     if params[:popup] == "true"
-      @related_items = @related_items.first(20)
+      @related_items = CarGroup.where(:id => related_item_ids)
+      # @related_items = related_item_ids.collect {|id| @related_items.detect {|x| x.id == id}}
+      @related_items = @related_items.uniq.compact
+      # @related_items = @related_items.first(20)
+      # @related_items = @related_items.select {|each_item| each_item.is_a?(CarGroup)}
       # @related_items = Item.last(20)
       return render :partial => "related_items_list", :layout => false
     else
+      @related_items = Item.where(:id => related_item_ids)
+      @related_items = related_item_ids.collect {|id| @related_items.detect {|x| x.id == id}}
+      @related_items = @related_items.uniq.compact
+
       @related_items = @related_items.paginate(:page => params[:page], :per_page => 10)
     end
 
