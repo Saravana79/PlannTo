056820ac49e0e3c134567bbf7d1be38a32abf3dd
Sivakumar <siva@plannto.com>
2016-09-20 11:10:07 +0530

    mongodb fixes

--- config/initializers/custom_require.rb
+++ config/initializers/custom_require.rb
@@ -5,7 +5,8 @@ if Rails.env.production?
   ENV['HEROKU_PASSWORD'] = "sivaplannto"
   ENV["HEROKU_APP"] = "plannto"
   # ENV["MONGOLAB_URI"] = "mongodb://planntonew:planntonew@ds051061-a0.mongolab.com:51061,ds051061-a1.mongolab.com:51061/planntonew"
-  ENV["MONGOLAB_URI"] = "mongodb://planntomongo:planntomongo@ds153975-a0.mlab.com:53975,ds153975-a1.mlab.com:53975/planntonew2612?replicaSet=rs-ds153975"
+  # ENV["MONGOLAB_URI"] = "mongodb://planntomongo:planntomongo@ds153975-a0.mlab.com:53975,ds153975-a1.mlab.com:53975/planntonew2612?replicaSet=rs-ds153975"
+  ENV["MONGODB_URI"] = "mongodb://heroku_qkhxcdvt:hs7vretjpfcunh0026a53oie9o@ds035836.mlab.com:35836/heroku_qkhxcdvt"
 
   require "#{Rails.root}/lib/heroku_resque_auto_scale.rb"
 
