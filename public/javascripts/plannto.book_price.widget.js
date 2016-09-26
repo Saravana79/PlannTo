c4ab00bb0f5daac62f092cb95156ec44120fa6f3
Saravana <saravanak@gmail.com>
2016-09-26 11:42:10 +0530

    modified the local version reference.

--- public/javascripts/plannto.book_price.widget.js
+++ public/javascripts/plannto.book_price.widget.js
@@ -10,9 +10,9 @@ var PlannTo = (function(window,undefined) {
     var PlannTo ={};
     var SubPath="/book_price_widget.js"
 //for production
- //   var domain = "www.plannto.com";
+   var domain = "www.plannto.com";
 //for development
-var domain = "localhost:3000";
+//var domain = "localhost:3000";
 // Localize jQuery variable
     var jQuery;
 
