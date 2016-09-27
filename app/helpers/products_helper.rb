312e42b9f72e4e969826fa1240518d82475d5623
Saravana <saravanak@gmail.com>
2016-09-28 00:10:39 +0530

    type 7 style changed

--- app/helpers/products_helper.rb
+++ app/helpers/products_helper.rb
@@ -112,12 +112,12 @@ module ProductsHelper
     if each_item.end_time.to_date == Date.today
       remaining_time = each_item.end_time.hour - Time.now.hour
       if (remaining_time > 8)
-        return_val = " Only for Today."
+        return_val = " Expires Today."
       else
         return_val = " Expires in #{remaining_time} hours."
       end
     else
-      return_val = ""
+      return_val = "Expires Soon."
     end
     return_val
   end
